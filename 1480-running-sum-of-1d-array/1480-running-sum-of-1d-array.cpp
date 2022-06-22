88b9ab06f0c6ce2c5fbe62aa813e8b2210abd09f
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-06-22 13:48:29 +0530

    Time: 8 ms (22.16%), Space: 8.5 MB (73.26%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> runningSum(vector<int>& nums) 
     {
         
-       partial_sum(begin(nums), end(nums), begin(nums));
-    return nums;
+       for(int i=1;i<nums.size();i++) nums[i] += nums[i-1];
+		return nums;
     }
 };
