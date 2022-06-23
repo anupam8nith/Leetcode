7376c189a74aad216a26b2925475c0f74a8af86e
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-06-23 23:18:04 +0530

    Time: 301 ms (72.62%), Space: 56.3 MB (41.31%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -2,21 +2,49 @@ class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) 
     {
-         if(courses.size() <= 0) return 0;
-        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
-            return a[1] < b[1];
+        // solution 1
+        
+        //  if(courses.size() <= 0) return 0;
+        // sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
+        //     return a[1] < b[1];
+        // });
+        // priority_queue<int> que;
+        // int sum = 0;
+        // for(auto i : courses) {
+        //     sum += i[0];
+        //     que.push(i[0]);
+        //     if(sum > i[1]) {
+        //         sum -= que.top();
+        //         que.pop();
+        //     }
+        // }
+        // return que.size();
+        
+        //solution 2
+        
+        sort(begin(courses),end(courses),[](auto & a,auto & b){
+            return a[1]<b[1];
         });
-        priority_queue<int> que;
-        int sum = 0;
-        for(auto i : courses) {
-            sum += i[0];
-            que.push(i[0]);
-            if(sum > i[1]) {
-                sum -= que.top();
-                que.pop();
+        priority_queue<int>pq;
+        int time=0;
+        for(auto & i:courses){
+            int dur=i[0],last=i[1];
+            if(time+dur<=last){
+                pq.push(dur);
+                time+=dur;
+            }
+            else{
+                if(!pq.empty()){
+                    int top=pq.top();
+                    if(top>dur){
+                    time-=top;
+                    pq.pop();
+                    time+=dur;
+                    pq.push(dur);
+                    }
+                }
             }
         }
-        return que.size();
-        
+        return pq.size();
     }
 };
