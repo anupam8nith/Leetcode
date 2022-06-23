856409a3d26167049ded684286949a699d12ca73
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-06-23 22:36:11 +0530

    Time: 0 ms (100.00%), Space: 6 MB (24.87%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -11,3 +11,20 @@ public:
             return false;
     }
 };
+
+// if (n==1){
+//             return true;
+//         }
+//         else if(n==0){
+//                 return false;
+//             } 
+//         while(n!=0){
+//             if(n==1){
+//                 return true;
+//             }
+//             else if((n%2)!=0){
+//                 return false;
+//             }
+//             n=n/2;
+//         }
+//         return true;
