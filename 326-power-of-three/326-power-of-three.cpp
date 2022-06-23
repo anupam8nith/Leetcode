6b7b870e97746520f78e21c11fafdc4d0b752124
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-06-23 22:53:13 +0530

    Time: 31 ms (54.21%), Space: 5.9 MB (72.05%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -5,11 +5,14 @@ public:
         // if(n<= 0)return false;
         // return 1162261467%n == 0;
         
-        while(n){
-            if(n==1) return true;
-            if(n%3!=0) return false;
-            n/=3;
+        if (n < 1) {
+            return false;
         }
-        return false;
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
     }
 };
