9cf827e8f146308cb0c7657f80d4f1ffc0ae2a9f
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-06-26 20:39:51 +0530

    Time: 142 ms (6.95%), Space: 45.8 MB (8.03%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) 
+    {
+        int sum = 0;
+        int n = cardPoints.size();
+        
+        vector<int> cummulativeSumFromFront(n+1, 0);
+        vector<int> cummulativeSumFromBehind(n+1, 0);
+        
+        sum = 0;
+        for (int i=0; i<n; i++) {
+            sum += cardPoints[i];
+            cummulativeSumFromFront[i+1] = sum;
+        }
+        sum = 0;
+        for (int i=n-1; i>=0; i--) {
+            sum += cardPoints[i];
+            cummulativeSumFromBehind[i] = sum;
+        }
+        
+        // Reversing is optional. I reversed it so that it would be easy
+        // to access sum of last (k-i) elements by just indexing at [k-i]
+        // Otherwise, I would have had to index it at [n-k+i] which would
+        // have made it difficult to read.
+        reverse(cummulativeSumFromBehind.begin(), cummulativeSumFromBehind.end());
+        
+        int answer = 0;
+        for(int i=0; i<=k; i++) {      
+            answer = max(answer, 
+                           cummulativeSumFromFront[i] // Sum of first 'i' cards.
+                         + cummulativeSumFromBehind[k-i]); // Sum of last 'k-i' cards.
+        }
+        return answer;
+    }
+};
+    
+    
+    //debug this
+    
+// long int i=0,count=0,sum=0,n=cardPoints.size();
+        
+//          vector<int>::iterator start=cardPoints.begin(), end = cardPoints.end();
+        
+//         while(count<k)
+//         {
+//            if(*start>*end)
+//            {
+//                sum+= *start;
+//                cardPoints.erase(start);
+//                count++;
+               
+//                if(count==k)
+//                      break;
+//            }
+//             else if(*start<*end)
+//             {
+//                 sum+=*end;
+//                 cardPoints.erase(end);
+//                 count++;
+                
+//                 if(count==k)
+//                      break;
+//             }
+            
+//              else if(*start==*end)
+//              {
+//                  vector<int>::iterator it1,it2;
+                 
+//                  it1=++start;
+//                  it2=--end;
+                 
+//                  while(*it1==*it2)
+//                  {
+//                      it1++;
+//                      it2--;
+//                  }
+                 
+//                  if(*it1>*it2)
+//                  {
+//                      sum+= *start;
+//                cardPoints.erase(start);
+//                count++;
+//                  }
+//                  else
+//                  {
+//                      sum+=*end;
+//                 cardPoints.erase(end);
+//                 count++;
+//                  }
+                 
+//                  if(count==k)
+//                      break;
+//              }
+        
+//         }
+//         return sum;
