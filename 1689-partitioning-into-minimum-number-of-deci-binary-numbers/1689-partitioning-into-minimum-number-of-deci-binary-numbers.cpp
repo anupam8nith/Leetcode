5b571bcb8d52cc4d26cd5eb83b82dc617024ef7d
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-06-28 00:06:51 +0530

    Time: 22 ms (98.10%), Space: 13.7 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,7 +1,34 @@
 class Solution {
+    
 public:
-    int minPartitions(string n) 
-    {
-        return *max_element(begin(n), end(n)) - '0'; //Just return max digit
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
+    int minPartitions(string n) {
+        int max = 0;
+        int i = 0;
+        int len = n.size();
+        for (; i < len; i++) {
+            int num =  n[i] - 48;
+            if (num == 9) {
+                return 9;
+            }
+            if (num > max) {
+                max = num;
+            } 
+            
+        }
+        return max;
     }
 };
+
+//solution 1
+
+// class Solution {
+// public:
+//     int minPartitions(string n) 
+//     {
+//         return *max_element(begin(n), end(n)) - '0'; //Just return max digit
+//     }
+// };
