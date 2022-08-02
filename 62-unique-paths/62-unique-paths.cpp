818d1bd34be9fa5db228fcb233a2031274fe5a30
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-02 21:08:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.97%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,15 @@
+
+   class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int k=m+n-2;
+      long  ans=1;
+        for(int i=1;i<=n-1;++i){
+            
+            
+           ans=ans*(k-(n-1)+i)/i;
+        }
+        return (int)ans;
+        
+    }
+};
