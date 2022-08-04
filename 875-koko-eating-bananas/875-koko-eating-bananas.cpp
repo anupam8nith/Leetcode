9ae9961407f6c2e2c26d84020d144021bb0eeb76
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-04 21:40:09 +0530

    Time: 80 ms (44.22%), Space: 18.8 MB (94.79%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -24,44 +24,9 @@ public:
             if(sum<=h)
                 high = mid;
             else
-            {
-                low = mid+1;
-            }
+           low = mid+1;
         }
         
         return low;
     }
 };
-
-
-// long long int sum=0,minn=0;
-    
-        
-//         minn = *min_element(piles.begin(),piles.end()); 
-        
-//         while(sum!=h)
-//         { 
-            // sum=0;
-            // for(long int i=0;i<n;i++)
-            // {
-            //     sum+= (piles[i]+minn-1)/minn;
-            // }
-        
-//             if(sum==h)
-//              return minn;
-//             else //decide minn value by binary search
-//             {
-//                 long int low= minn, high= *max_element(piles.begin(),piles.end());
-//                 long int mid = low + (high-low)/2;
-                
-//                 if(sum==h)
-//              return minn;
-//                 else if(sum>h)
-//                     high = mid;
-//                 else if(sum<h)
-//                     low = mid;
-                
-//                 minn= mid;
-//             }
-//         }
-        
