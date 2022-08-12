3a2cbc08eacc0428262e84eb7a7cb0eb314e6d9b
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-12 19:13:03 +0530

    Time: 112 ms (83.61%), Space: 51.4 MB (51.65%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        unordered_map <int,int> um;
+        
+        for(long int i=0;i<nums.size();i++)
+        {
+            um[nums[i]]++;
+            if(um[nums[i]]>1)
+                return true;
+        }
+        
+        return false;
+    }
+};
