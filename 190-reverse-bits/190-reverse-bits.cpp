7556ff6b4f19ff89671e0ab16f7d888a31e9155c
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-13 09:39:46 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (11.24%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -2,11 +2,22 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) 
     {
-        uint32_t m = 0;
-    for (int i = 0; i < 32; i++, n >>= 1) {
-        m <<= 1;
-        m |= n & 1;
-    }
-    return m;
+        
+        cout<<n<<endl;
+        uint32_t ans;
+        string s = "";
+        int i = 1;
+        while(i<=32){
+            if(n & 1){
+                s += '1';
+            }else{
+                s += '0';
+            }
+            i++;
+            n = n >> 1;
+        
+        }
+        
+        return stoul(s,0,2);
     }
 };
