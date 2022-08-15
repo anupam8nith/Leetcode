46382cf091f88c87a3d1a068db5a845177f8ec45
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-16 01:49:17 +0530

    Time: 102 ms (99.92%), Space: 93.5 MB (9.25%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -6,19 +6,42 @@ public:
         cin.tie(0);
         cout.tie(0);
         
-        
-      int select=0, profit=INT_MIN,max_profit=0;
-        
-        
-        
-        for(long int i=0;i<prices.size();i++)
-        {
-            if(prices[select]>prices[i])
-                select=i;
-            else
-                max_profit= max(max_profit, prices[i]-prices[select]);
+        int n = prices.size();
+        if (n < 2) {
+            return 0;
         }
+        int min_price = prices[0];
         
-        return max_profit;
+        int ans = 0;
+        for (int i=1; i<n; i++) {
+            if (prices[i] >= min_price) {
+                ans = max(ans, prices[i] - min_price);
+            } else {
+                min_price = min(min_price, prices[i]);
+            }
+        }
+        return ans;
     }
 };
+
+
+//code 1
+
+// ios::sync_with_stdio(false);
+//         cin.tie(0);
+//         cout.tie(0);
+        
+        
+//       int select=0, profit=INT_MIN,max_profit=0;
+        
+        
+        
+//         for(long int i=0;i<prices.size();i++)
+//         {
+//             if(prices[select]>prices[i])
+//                 select=i;
+//             else
+//                 max_profit= max(max_profit, prices[i]-prices[select]);
+//         }
+        
+//         return max_profit;
