c300063fd5bd625bff3f8318f736d0e79e390436
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-17 05:14:33 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (26.88%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n)
+    bool isPowerOfTwo(int n) 
     {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         if(n==0 || n<0)
             return false;
         
-        if(ceil(log2(n)) == floor(log2(n)))
-            return true;
-        else
-            return false;
+        int32_t m=n; 
+        int count=0;
+        
+        for(int i=0;i<32;i++)
+        {
+            if(count>1)
+                return false;
+            if(m&1==1)
+                count++;
+            m=m>>1;
+        }
+        
+        return true;
     }
 };
 
-// if (n==1){
+// my code 1 0ms solution
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) 
+//     {
+// if(n==0 || n<0)
+//             return false;
+        
+//         if(ceil(log2(n)) == floor(log2(n)))
 //             return true;
-//         }
-//         else if(n==0){
-//                 return false;
-//             } 
-//         while(n!=0){
-//             if(n==1){
-//                 return true;
-//             }
-//             else if((n%2)!=0){
-//                 return false;
-//             }
-//             n=n/2;
-//         }
-//         return true;
+//         else
+//             return false;
+//    }
+//  };
