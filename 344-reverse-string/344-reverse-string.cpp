5440c0207c3250f594fa530d12f30b943b214aff
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-17 03:39:29 +0530

    Time: 18 ms (96.99%), Space: 23.4 MB (41.00%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -4,8 +4,21 @@ public:
     {
         std::ios_base::sync_with_stdio(false);
         cin.tie(NULL);
+        //code
         
-        reverse(s.begin(),s.end());
-        return;
+        long int n = s.size();
+        
+        for(long int i=0;i<n/2;i++)
+            swap(s[i],s[n-1-i]);
+        
+       return;
     }
 };
+
+//code1 18ms using STL
+
+// std::ios_base::sync_with_stdio(false);
+//         cin.tie(NULL);
+        
+//         reverse(s.begin(),s.end());
+//         return;
