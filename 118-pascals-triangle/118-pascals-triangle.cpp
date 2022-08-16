4e3588ee1a1185e728d5563f096cde732bcae31f
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-17 00:31:55 +0530

    Time: 3 ms (39.43%), Space: 6.6 MB (32.76%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int nr) 
+    vector<vector<int>> generate(int numRows) 
     {
         ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+        cin.tie(NULL);
 
-        vector<vector<int>> answer;
-        for(int i=0;i<nr;i++)
-        {
-            vector<int> ans(i+1);
-            for(int j=0;j<=i;j++)
-            {
-                if(i==0 || j==0 || j==i)
-                    ans[j]=1;
-                else
-                {
-                    ans[j] = answer[i-1][j-1]+answer[i-1][j];
-                }
-                
+        std::vector<vector<int>>ans{};
+        
+        for(int i = 0; i < numRows; i++){
+            std::vector<int>row (i+1,1);
+            
+            for(int j = 1; j < row.size()-1; j++){
+                row.at(j) = ans[i-1][j] + ans[i-1][j-1];
             }
-            answer.push_back(ans);
-            ans.clear();
+            
+            ans.push_back(row);
         }
         
-        return  answer;
+        return ans;
     }
 };
+
+
+// my code 0ms accepted
+
+// ios_base::sync_with_stdio(false);
+// cin.tie(NULL);
+
+//         vector<vector<int>> answer;
+//         for(int i=0;i<nr;i++)
+//         {
+//             vector<int> ans(i+1);
+//             for(int j=0;j<=i;j++)
+//             {
+//                 if(i==0 || j==0 || j==i)
+//                     ans[j]=1;
+//                 else
+//                 {
+//                     ans[j] = answer[i-1][j-1]+answer[i-1][j];
+//                 }
+                
+//             }
+//             answer.push_back(ans);
+//             ans.clear();
+//         }
+        
+//         return  answer;
