ad549d97a92377dc6a59aa642702ce63869c2b4b
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-17 02:57:46 +0530

    Time: 25 ms (88.72%), Space: 19.1 MB (83.40%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -5,24 +5,37 @@ public:
         std::ios_base::sync_with_stdio(false);
         cin.tie(NULL);
             
-        int count=0;
-        
-        auto it = nums.begin();
-        
-        while(it!=nums.end())
+        int nonzero=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(*it==0)
-            {
-                nums.erase(it);
-                count++;
-            }
-            if(*it==0)
-                it--;
-            it++;
+            if(nums[i]!=0)
+                swap(nums[nonzero++],nums[i]);
         }
-        
-        while(count--)
-            nums.push_back(0);
-        return;
     }
 };
+
+
+//my code 110 ms AC
+
+// std::ios_base::sync_with_stdio(false);
+//         cin.tie(NULL);
+            
+//         int count=0;
+        
+//         auto it = nums.begin();
+        
+//         while(it!=nums.end())
+//         {
+//             if(*it==0)
+//             {
+//                 nums.erase(it);
+//                 count++;
+//             }
+//             if(*it==0)
+//                 it--;
+//             it++;
+//         }
+        
+//         while(count--)
+//             nums.push_back(0);
+//         return;
