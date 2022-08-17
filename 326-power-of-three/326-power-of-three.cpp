bb472fa95a3c23df87ad1518dc921e57a24a498e
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-17 05:32:28 +0530

    Time: 12 ms (97.82%), Space: 6.1 MB (12.98%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
-    
-    
-    bool solve(int m)
-    {
-        if(m==3 || m==1)
-            return true;
-        
-        if(m%3==0)
-            return solve(m/3);
-        else
-            return false;
-        
-        
-    }
     bool isPowerOfThree(int n) 
     {
-        std::ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
         
-         if(n<0 || n==0)
-            return false;
-        return solve(n);
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        if (n <= 0)
+        return false;
+    if (n % 3 == 0)
+        return isPowerOfThree(n / 3);
+    if (n == 1)
+        return true;
+    return false;
     }
 };
+
+
+
+//Mycode 12ms
+
+// class Solution {
+// public:
+    
+    
+//     bool solve(int m)
+//     {
+//         if(m==3 || m==1)
+//             return true;
+        
+//         if(m%3==0)
+//             return solve(m/3);
+//         else
+//             return false;
+        
+        
+//     }
+//     bool isPowerOfThree(int n) 
+//     {
+//         std::ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+        
+//          if(n<0 || n==0)
+//             return false;
+//         return solve(n);
+//     }
+// };
