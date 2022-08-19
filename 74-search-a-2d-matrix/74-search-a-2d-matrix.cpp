539265185062496c7e98f78beeea02ddb4ef270a
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-20 03:08:35 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (5.02%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) 
     {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-            
-        
+
         for(auto x: matrix)
         {
             if(x.at(0)<= target && x.at(x.size()-1)>=target)
