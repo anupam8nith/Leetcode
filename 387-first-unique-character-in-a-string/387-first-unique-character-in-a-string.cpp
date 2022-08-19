7d4600adf4ad999d21b8843d1b1148d97f5193c2
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-20 03:14:37 +0530

    Time: 38 ms (74.94%), Space: 10.8 MB (44.39%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -5,21 +5,45 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         
-        unordered_map<char,int> um1,um2;
+        unordered_map<char,int> um;
         
-        for(int i=s.length()-1;i>=0;i--)
-        {
-            um1[s[i]]++; //freq
-            um2[s[i]] = i; //index;
-        }
+       for(long int i=0;i<s.size();i++) um[s[i]]++;
         
-        for(auto ch: um1)
-        {
-            if(ch.second==1)
-             return um2[ch.first];
-        }
+       for(long int i=0;i<s.size();i++)
+       {
+           if(um[s[i]]==1)
+               return i;
+       }
         
         return -1;
         
     }
 };
+
+//51 ms 
+
+// class Solution {
+// public:
+//     int firstUniqChar(string s) 
+//     {
+//         ios_base::sync_with_stdio(false);
+//         cin.tie(NULL);
+        
+//         unordered_map<char,int> um1,um2;
+        
+//         for(int i=s.length()-1;i>=0;i--)
+//         {
+//             um1[s[i]]++; //freq
+//             um2[s[i]] = i; //index;
+//         }
+        
+//         for(auto ch: um1)
+//         {
+//             if(ch.second==1)
+//              return um2[ch.first];
+//         }
+        
+//         return -1;
+        
+//     }
+// };
