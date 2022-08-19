4beec234d4268877448a48c49ecbd96f76ea5a20
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-20 03:22:11 +0530

    Time: 21 ms (65.89%), Space: 9 MB (6.70%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canConstruct(string ransom, string magazine) 
+    {
+        //store the freq of both
+        
+        unordered_map<char,int> um1,um2;
+        
+        for(long int i=0;i<magazine.size();i++)
+            um2[magazine[i]]++;
+        
+        for(long int i=0;i<ransom.size();i++)
+            um1[ransom[i]]++;
+        
+        //check if freq of magazine >= ransome 
+        
+        for(long int i=0;i<ransom.size();i++)
+        {
+            if(um2[ransom[i]]<um1[ransom[i]])
+                return false;
+        }
+        return true;
+        
+    }
+};
