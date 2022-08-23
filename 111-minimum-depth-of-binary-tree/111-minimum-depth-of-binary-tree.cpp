5072464e1895b58c7994dbd4d200a81b13b29545
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-23 07:30:39 +0530

    Time: 242 ms (99.73%), Space: 97.4 MB (99.98%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -11,43 +11,66 @@
  */
 class Solution {
 public:
-    int minDepth(TreeNode* root) 
-    {
-        //fast io
+    int minDepth(TreeNode* root) {
+        //referred
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        
-        
-        if(root==NULL)
+        if (!root)
             return 0;
-        
-        int height=0;
-        // code 
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        //loop
-        while(!q.empty())
-        {
-            int size = q.size();
-            
-            for(int i=0;i<size;i++)
-            {
-                TreeNode* curr_node = q.front();
-                q.pop();
-                if(curr_node->left==NULL && curr_node->right==NULL)
-                    return height+1;
-                if(curr_node->left!=NULL) q.push(curr_node->left);
-                if(curr_node->right!=NULL) q.push(curr_node->right);
-                
-                
-            }
-            
-            height++;
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+        if(!root->left || !root->right){
+            return !root->left ? right + 1 : left + 1;
         }
-        
-        
-        
-        return height;
+        root->left = NULL;
+        root->right = NULL;
+        return min(left, right)+1;
+
     }
 };
+
+
+//my solution 251 ms
+
+// class Solution {
+// public:
+//     int minDepth(TreeNode* root) 
+//     {
+        
+//         ios_base::sync_with_stdio(false);
+//         cin.tie(NULL);
+        
+        
+//         if(root==NULL)
+//             return 0;
+        
+//         int height=0;
+//         // code 
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         //loop
+//         while(!q.empty())
+//         {
+//             int size = q.size();
+            
+//             for(int i=0;i<size;i++)
+//             {
+//                 TreeNode* curr_node = q.front();
+//                 q.pop();
+//                 if(curr_node->left==NULL && curr_node->right==NULL)
+//                     return height+1;
+//                 if(curr_node->left!=NULL) q.push(curr_node->left);
+//                 if(curr_node->right!=NULL) q.push(curr_node->right);
+                
+                
+//             }
+            
+//             height++;
+//         }
+        
+        
+        
+//         return height;
+//     }
+// };
