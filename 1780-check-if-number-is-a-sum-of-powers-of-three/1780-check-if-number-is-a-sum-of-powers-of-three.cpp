22ab7fd1f0ecadcc863b2ffb87d9d36093b0069c
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-25 10:50:04 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (48.16%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,33 +1,54 @@
 class Solution {
 public:
-    bool checkPowersOfThree(long int n) 
-    {
-        // ios_base::sync_with_stdio(false);
-        // cin.tie(NULL);
-        
-        if(n==1 || n==3)
-            return true;
-        if(n==2 )
-            return false;
-        
-        long int val = 1;
-        
-        while(val<=n)
-            
-        {
-            val*=3;
-        }
-        val/=3;
-        
-        if(val==n)
-            return true;
-        
-        if(n>=2*val)
-            return false;
-        else
-            
-            n=n-val;
-        
-        return checkPowersOfThree(n);
+    string tern(int n){
+        string s = "";
+        if(n / 3 != 0) s += tern(n / 3);
+        s += to_string(n%3);
+        return s;
+    }
+
+    bool checkPowersOfThree(int n) {
+		if(n <= 0) return false;
+		string t = tern(n);
+		for(auto i: t) if(i == '2') return false;
+		return true;
     }
 };
+
+
+
+// sol1
+
+// class Solution {
+// public:
+//     bool checkPowersOfThree(long int n) 
+//     {
+//         // ios_base::sync_with_stdio(false);
+//         // cin.tie(NULL);
+        
+//         if(n==1 || n==3)
+//             return true;
+//         if(n==2 )
+//             return false;
+        
+//         long int val = 1;
+        
+//         while(val<=n)
+            
+//         {
+//             val*=3;
+//         }
+//         val/=3;
+        
+//         if(val==n)
+//             return true;
+        
+//         if(n>=2*val)
+//             return false;
+//         else
+            
+//             n=n-val;
+        
+//         return checkPowersOfThree(n);
+//     }
+// };
