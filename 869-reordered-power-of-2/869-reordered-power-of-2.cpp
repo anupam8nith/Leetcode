1a27c0cdc6e661288be4ba2bfefc4e6bfd59f027
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-08-26 12:49:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (83.90%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     bool reorderedPowerOf2(int N) 
     {
-         unordered_set<string> powerOfTwos;
+         // unordered_set<string> powerOfTwos;
+        string str = to_string(N);
+        sort(str.begin(), str.end());
         
          for (int i = 0; i < 32; ++i) 
          {
              int n = 1 << i;
+             
              string s = to_string(n);
              sort(s.begin(), s.end());
-             powerOfTwos.insert(s);
+             if(str == s){
+                 return true;
+             }
          }
-        
-         string s = to_string(N);
-         sort(s.begin(), s.end());
-         return powerOfTwos.count(s) > 0;
-        
+        return false;
     }
 };
 
