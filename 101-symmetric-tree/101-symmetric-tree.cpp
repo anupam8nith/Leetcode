2eec32715da3b7674c46b036acbc7a80125161d1
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-02 16:03:29 +0530

    Time: 19 ms (5.10%), Space: 16.6 MB (8.78%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -46,10 +46,50 @@ public:
         }
         return true;
     }
+    
+   
 };
 
 
-//best iterative
+                                //my bfs solution
+
+// bool isSymmetric(TreeNode* root) 
+//     {
+//         if(root->left==NULL && root->right==NULL)
+//             return true ;
+        
+//         TreeNode *p=root->left,*q=root->right;
+        
+//         //iteratively using bfs
+        
+//         if(!p ^ !q)
+//             return false;
+        
+        
+//         //BFS
+//         queue<pair<TreeNode*,TreeNode*>> kyu;
+//         kyu.push({p,q});
+        
+        
+//         while(!kyu.empty())
+//         {
+//             p= kyu.front().first;
+//             q= kyu.front().second;
+//             kyu.pop();
+            
+//             if(p->val!=q->val) return false;
+//             if(!p->left ^ !q->right) return false;
+//             if(!p->right ^ !q->left) return false;
+            
+            
+//             if(p->left!=NULL && q->right!=NULL) kyu.push({p->left,q->right});
+//             if(p->right!=NULL && q->left!=NULL) kyu.push({p->right,q->left});
+            
+//         }
+//         return true;
+//     }
+
+                                    //best iterative
 
 // bool isSymmetric(TreeNode* root) {
         
