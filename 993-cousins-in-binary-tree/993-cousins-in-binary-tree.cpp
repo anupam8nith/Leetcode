7b60635fd70ec4e34e1899b7d2dc389c57727d15
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-03 01:06:33 +0530

    Time: 11 ms (14.68%), Space: 13.2 MB (5.19%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
@@ -11,46 +11,114 @@
  */
 class Solution {
 public:
-    bool isCousins(TreeNode* root, int x, int y) 
+   
+    unordered_map<int, pair<int,int>> mp;
+    
+	void solve(TreeNode* root, int level, int parent)
     {
-        //BFS
-        
-        queue<TreeNode*> q;
-    q.push(root);
-    
-    while(!q.empty())
+
+		if(root == NULL){
+			return;
+		}
+
+		mp[root->val] = {level, parent};
+
+		solve(root->left, level + 1, root->val);
+		solve(root->right, level + 1, root->val);
+	}
+
+	bool isCousins(TreeNode* root, int x, int y) 
     {
-        int size = q.size();
-        bool foundX = false;
-        bool foundY = false;
-        
-        for(int i=0;i<size;i++)
-        {
-            TreeNode* curr = q.front();
-            q.pop();
-            if(curr->val == x)
-                foundX = true;
-            if(curr->val == y)
-                foundY = true;
-            
-            if(curr->left && curr->right)  // check if x and y are children of the same parent 
-            {
-                if((curr->left->val == x && curr->right->val == y) || (curr->left->val == y && curr->right->val == x))
-                    return false;
-            }
-            
-            if(curr->left)
-                q.push(curr->left);
-            
-            if(curr->right)
-                q.push(curr->right);
-        }
-        
-        if(foundX && foundY)  // if x and y are cousins 
-            return true;
-    }
+		solve(root, 0, -1);
+
+		if(mp[x].first == mp[y].first && mp[x].second != mp[y].second){
+			return true;
+		}
+
+		return false;
+	}
     
-    return false;
-}
+    };
     
-};
+// DFS solution
+
+// class Solution {
+// public:
+
+// 	unordered_map<int, pair<int,int>> mp;
+// 	void solve(TreeNode* root, int level, int parent){
+
+// 		if(root == NULL){
+// 			return;
+// 		}
+
+// 		mp[root->val] = {level, parent};
+
+// 		solve(root->left, level + 1, root->val);
+// 		solve(root->right, level + 1, root->val);
+// 	}
+
+// 	bool isCousins(TreeNode* root, int x, int y) {
+
+// 		solve(root, 0, -1);
+
+// 		if(mp[x].first == mp[y].first && mp[x].second != mp[y].second){
+// 			return true;
+// 		}
+
+// 		return false;
+// 	}
+// };
+
+
+
+
+
+
+// my solution: bfs
+
+// class Solution {
+// public:
+//     bool isCousins(TreeNode* root, int x, int y) 
+//     {
+//         //BFS
+        
+//         queue<TreeNode*> q;
+//     q.push(root);
+    
+//     while(!q.empty())
+//     {
+//         int size = q.size();
+//         bool foundX = false;
+//         bool foundY = false;
+        
+//         for(int i=0;i<size;i++)
+//         {
+//             TreeNode* curr = q.front();
+//             q.pop();
+//             if(curr->val == x)
+//                 foundX = true;
+//             if(curr->val == y)
+//                 foundY = true;
+            
+//             if(curr->left && curr->right)  // check if x and y are children of the same parent 
+//             {
+//                 if((curr->left->val == x && curr->right->val == y) || (curr->left->val == y && curr->right->val == x))
+//                     return false;
+//             }
+            
+//             if(curr->left)
+//                 q.push(curr->left);
+            
+//             if(curr->right)
+//                 q.push(curr->right);
+//         }
+        
+//         if(foundX && foundY)  // if x and y are cousins 
+//             return true;
+//     }
+    
+//     return false;
+// }
+    
+// };
