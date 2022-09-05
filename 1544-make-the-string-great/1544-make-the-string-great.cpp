91ab34f8df2c650c72f4889de9f5fe545e23a072
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-05 16:27:50 +0530

    Time: 9 ms (15.78%), Space: 7.1 MB (7.89%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -25,3 +25,23 @@ public:
         return ans;
     }
 };
+
+// O(n*n)
+
+// while (sz != s.size()) {
+//         sz = s.size();
+//         for (int i = 0; i + 1 < s.size(); ++i)
+//             if (abs(s[i] - s[i + 1]) == 32)
+//                 s = s.substr(0, i) + s.substr(i + 2);
+//     }
+//     return s;
+
+// O(n)
+
+// for (int i = 0; i < s.size(); ++i) {
+//         if (p > 0 && abs(s[i] - s[p - 1]) == 32)
+//             --p;
+//         else
+//             s[p++] = s[i];
+//     }
+//     return s.substr(0, p);
