584bb0dc8e5061b3b50ed066f8c339b1fe405df5
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-08 23:45:33 +0530

    Time: 4 ms (30.91%), Space: 6.9 MB (89.63%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -27,6 +27,7 @@ public:
         if (output.empty())
             while (input.size())
                 output.push(input.top()), input.pop();
+        
         return output.top();
         
     }
@@ -36,6 +37,11 @@ public:
         
     }
 };
+
+// to understand
+// I have one input stack, onto which I push the incoming elements, and one output stack, from which I peek/pop. I move elements from input stack to output stack when needed, i.e., when I need to peek/pop but the output stack is empty. When that happens, I move all elements from input to output stack, thereby reversing the order so it's the correct order for peek/pop.
+
+// The loop in peek does the moving from input to output stack. Each element only ever gets moved like that once, though, and only after we already spent time pushing it, so the overall amortized cost for each operation is O(1).
 
 /**
  * Your MyQueue object will be instantiated and called as such:
