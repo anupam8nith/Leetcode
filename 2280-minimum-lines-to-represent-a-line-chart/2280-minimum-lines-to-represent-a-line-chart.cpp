d5b9559deccb824fd72bc6ba00572a78e9c67523
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-08 22:30:30 +0530

    Time: 908 ms (21.99%), Space: 100.7 MB (94.57%) - LeetHub

---
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    
+    int minimumLines(vector<vector<int>>& stockPrices) 
+    {
+        int n = stockPrices.size();
+        
+        if(n<=1)
+            return 0;
+        int count=1;
+        
+        sort(stockPrices.begin(),stockPrices.end());
+        
+        for(int i=1;i<n-1;i++)
+        {
+            long double slope1 = (long double) (stockPrices[i][1] -  stockPrices[i-1][1])*(stockPrices[i+1][0] -  stockPrices[i][0]);
+            
+             long double slope2 = (long double) (stockPrices[i+1][1] -  stockPrices[i][1])*(stockPrices[i][0] -  stockPrices[i-1][0]);
+            
+            if(slope1!=slope2)
+                count+=1;
+        }
+        
+        return count;
+        
+        
+    }
+};
+
