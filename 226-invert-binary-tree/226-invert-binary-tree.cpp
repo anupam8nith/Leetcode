935f299fbb7c39d60437db8ff1a10096e0b8b4de
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-09 01:06:47 +0530

    Time: 4 ms (49.01%), Space: 9.9 MB (5.70%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -9,45 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) 
-    {
-        //recursive
-        if(!root)return NULL;
+// class Solution {
+// public:
+//     TreeNode* invertTree(TreeNode* root) 
+//     {
+//         //recursive
+//         if(!root)return NULL;
         
-        invertTree(root->left);
-        invertTree(root->right);
+//         invertTree(root->left);
+//         invertTree(root->right);
         
-        swap(root->left,root->right);
+//         swap(root->left,root->right);
         
-        return root;
-    }
-};
+//         return root;
+//     }
+// };
 
 
 //Iterative
 
 
-// class Solution {
-// public:
-//     TreeNode* invertTree(TreeNode* root) {
-//         if(!root) return NULL;
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
         
-//         stack<TreeNode*> st;        
-//         st.push(root);
+        stack<TreeNode*> st;        
+        st.push(root);
         
-//         while(st.size() > 0){
-//             TreeNode* top = st.top();
-//             st.pop();
+        while(st.size() > 0){
+            TreeNode* top = st.top();
+            st.pop();
             
-//             if(top->left) st.push(top->left);
-//             if(top->right) st.push(top->right);
+            if(top->left) st.push(top->left);
+            if(top->right) st.push(top->right);
             
-//             // swap the left & right child of parent node
-//             swap(top->left, top->right);
-//         }
+            // swap the left & right child of parent node
+            swap(top->left, top->right);
+        }
         
-//         return root;
-//     }
-// };
+        return root;
+    }
+};
