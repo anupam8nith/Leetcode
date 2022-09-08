79ef19bba6abe3766a42dc9d7db036a76153c85a
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-09 01:20:59 +0530

    Time: 28 ms (11.22%), Space: 21.4 MB (38.77%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -14,11 +14,61 @@ public:
     
     bool hasPathSum(TreeNode* root, int sum) 
     {
-        //recursive
-        if(root==NULL) return false;
+        // manaual dfs 
+        stack <tuple <TreeNode*, int>> S;
+        int cur_total;
+        TreeNode *cur_node;
+        S.push(make_tuple(root, 0));
         
-         if (root->val == sum && root->left ==  NULL && root->right == NULL) return true;
-        return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+        while (!S.empty()) {
+            cur_node = get<0>(S.top());
+            cur_total = get<1>(S.top());
+            S.pop();
+            
+            if (!cur_node) {
+                continue;
+            } else if (!cur_node->left && !cur_node->right) {
+                if (cur_total + cur_node->val == sum) { return true; }
+                continue;
+            }
+            
+            S.push(make_tuple(cur_node->right, cur_total+cur_node->val)); 
+            S.push(make_tuple(cur_node->left, cur_total+cur_node->val));
+        }
         
+        return false;
+       
     }
 };
+
+
+ //recursive
+//         if(root==NULL) return false;
+        
+//          if (root->val == sum && root->left ==  NULL && root->right == NULL) return true;
+//         return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+
+
+//recrsive
+
+// if(root == NULL)  return false;
+//     if(root->right == root->left)  return sum == root->val;
+//     return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val) ;
+
+
+
+// recursive
+
+// if(root == NULL)
+//         {
+//             return false;
+//         }
+//         if(root->left == NULL && root->right == NULL && root->val - targetSum == 0)
+//         {
+//             return true;
+//         }
+        
+//         bool left = hasPathSum(root->left,targetSum - root->val);
+//         bool right = hasPathSum(root->right,targetSum - root->val);
+        
+//         return left || right;
