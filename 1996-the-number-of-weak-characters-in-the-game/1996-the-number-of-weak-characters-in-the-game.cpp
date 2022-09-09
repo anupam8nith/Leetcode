243df8ef3fd42e164a501aad9e00b66f46ebc7d5
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-09 21:40:54 +0530

    Time: 872 ms (72.71%), Space: 126.5 MB (35.16%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -31,6 +31,35 @@ public:
   }
 };
 
+// GREEDY approach
+// class Solution
+// {
+// public:
+//      static bool comp(vector<int> &a, vector<int> &b)
+//      {
+//           if (a[0] == b[0])
+//                  return a[1] > b[1] ;
+//           return 
+//                  a[0] < b[0];
+//      }
+  
+  
+//      int numberOfWeakCharacters(vector<vector<int>> &properties)
+//      {
+//           sort(properties.begin(), properties.end(), comp); //sorting the array
+//           int mx = 0 ;   //max till now while moving from right
+//           int c = 0;
+
+//           for (int i = properties.size() - 1; i >= 0; i--)
+//           {
+//              if (properties[i][1] < mx) // if the second parameter is also less increase count 
+//                     c++;
+//                mx = max(mx, properties[i][1]);
+//           }
+//           return c ;
+//      }
+// };
+
 
 /*
 this is the comparator class function which sort Attacks in Ascending order and if attacks are duplicate then the 
