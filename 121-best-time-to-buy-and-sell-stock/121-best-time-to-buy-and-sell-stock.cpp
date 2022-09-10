a9d341df99aa18bad2fa15c2cb6e72a3b32cf47d
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-11 03:56:20 +0530

    Time: 186 ms (62.87%), Space: 93.5 MB (8.84%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -24,6 +24,34 @@ public:
     }
 };
 
+//DP Solution
+// class Solution {
+// public:
+//     int dp[100001][3];
+//     int rec(vector<int>& prices,int i,int n,int k,bool buy)
+//     {
+
+//         if(i>=n or k<=0)
+//             return 0;
+//         if(dp[i][buy]!=-1) return dp[i][buy];
+        
+//         if(buy)//buy
+//         {
+//             return dp[i][buy]=max(-prices[i]+rec(prices,i+1,n,k,!buy),rec(prices,i+1,n,k,buy));
+//         }
+//         else//sell 
+//         {
+//              return dp[i][buy]=max(prices[i]+rec(prices,i+1,n,k-1,!buy),rec(prices,i+1,n,k,buy));
+//         }
+        
+//     }
+//     int maxProfit(vector<int>& prices) {
+//         memset(dp,-1,sizeof(dp));
+//         int n=prices.size();
+//         return rec(prices,0,n,1,1);
+//     }
+// };
+
 
 //code 1
 
