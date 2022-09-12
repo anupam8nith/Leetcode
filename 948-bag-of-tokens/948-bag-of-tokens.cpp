8a183f457552a3ccc4e4b4e4cc7e32dfcfc72d9f
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-12 09:00:54 +0530

    Time: 8 ms (72.03%), Space: 10.6 MB (66.86%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& token, int power) 
+    {
+        int score=0;
+        int n = token.size();
+        
+        if(n==1)
+        {
+            if(power<token[0])
+                return 0;
+            else
+                return 1;
+        }
+        
+        //
+        
+       sort(token.begin(),token.end());
+        
+       int l=0,r=n-1,ans=0;
+        
+        
+       while(l<=r)
+       {
+           if(power>=token[l])
+           {
+               power-=token[l];
+               score++;
+               ans=max(ans,score);
+               l++;
+           }
+           else if(score>0)
+           {
+               power+=token[r];
+               score--;
+               r--;
+           }
+           else
+           {
+               break;
+           }
+       }
+           
+           
+           
+           
+        return ans;
+    }
+};
