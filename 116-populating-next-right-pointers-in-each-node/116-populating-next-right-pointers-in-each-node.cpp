54f91283755544c45d4492a9df0905afdfbffafd
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-13 19:37:59 +0530

    Time: 37 ms (35.34%), Space: 17.7 MB (7.55%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -16,55 +16,6 @@ public:
 };
 */
 
-class Solution {
-public:
-    Node* connect(Node* root) 
-    {
-        if(root==NULL) return nullptr;
-        //BFS
-        queue<Node*> q;
-        
-        q.push(root);
-        q.push(NULL);
-        
-        while(!q.empty())
-        {
-                Node *curr=q.front();
-                q.pop();
-               
-               if(curr)
-               {
-                   if(!q.empty())
-                   {    
-                       curr->next=q.front();
-                   }
-                   else
-                   {
-                       curr->next=NULL;
-                       
-                   }
-                   
-                   if(curr->left)q.push(curr->left);
-                   if(curr->right)q.push(curr->right);
-                }
-                
-            else 
-            {
-                if(!q.empty())
-                    q.push(NULL);
-            }
-                
-        }
-        
-        return root;
-    }
-};
-
-
-
-// review
-
-
 // class Solution {
 // public:
 //     Node* connect(Node* root) 
@@ -74,36 +25,85 @@ public:
 //         queue<Node*> q;
         
 //         q.push(root);
+//         q.push(NULL);
         
 //         while(!q.empty())
 //         {
-//             int size=q.size();
-            
-//             vector<Node*> level;
-            
-//             for(int i=0;i<size;i++)
-//             {
 //                 Node *curr=q.front();
 //                 q.pop();
+               
+//                if(curr)
+//                {
+//                    if(!q.empty())
+//                    {    
+//                        curr->next=q.front();
+//                    }
+//                    else
+//                    {
+//                        curr->next=NULL;
+                       
+//                    }
+                   
+//                    if(curr->left)q.push(curr->left);
+//                    if(curr->right)q.push(curr->right);
+//                 }
                 
-//                 //operations
-//                 level.push_back(curr);
-                
-                
-//                 //store next level
-//                 if(!curr->left)q.push(curr->left);
-//                 if(!curr->right)q.push(curr->right);
-//             }
-            
-//             for(int j=0;j<=level.size()-1;j++)
+//             else 
 //             {
-//                 if(j!=level.size()-1)
-//                 level[j]->next=level[j+1];
-//                 else
-//                 level[j]->next=NULL;
+//                 if(!q.empty())
+//                     q.push(NULL);
 //             }
+                
 //         }
         
 //         return root;
 //     }
 // };
+
+
+
+// review
+
+
+class Solution {
+public:
+    Node* connect(Node* root) 
+    {
+        if(root==NULL) return nullptr;
+        //BFS
+        queue<Node*> q;
+        
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            int size=q.size();
+            
+            vector<Node*> level;
+            
+            for(int i=0;i<size;i++)
+            {
+                Node *curr=q.front();
+                q.pop();
+                
+                //operations
+                level.push_back(curr);
+                
+                
+                //store next level
+                if(curr->left)q.push(curr->left);
+                if(curr->right)q.push(curr->right);
+            }
+            
+            for(int j=0;j<=level.size()-1;j++)
+            {
+                if(j!=level.size()-1)
+                level[j]->next=level[j+1];
+                else
+                level[j]->next=NULL;
+            }
+        }
+        
+        return root;
+    }
+};
