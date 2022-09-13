5e1fdca9dfd22352983eb0d058052d291bf0608b
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-13 23:29:40 +0530

    Time: 6 ms (90.06%), Space: 8.3 MB (98.97%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -13,6 +13,33 @@ public:
     }
 };
 
+//
+// class Solution {
+// public:
+//     vector<vector<int>> permuteUnique(vector<int> &a) {
+      
+//         sort(a.begin(),a.end());
+//         int n=a.size();
+//         vector<vector<int>>res;
+//         res.push_back(a);
+//         while(true){
+//             int l=n-1;
+//             while(l>0&&a[l-1]>=a[l]) l--;
+//             if(l==0) break;
+//             int k=l-1;
+//             l=n-1;
+//             while(a[k]>=a[l]) l--;
+//             swap (a[k],a[l]);
+//             sort(a.begin()+k+1,a.end());
+//             res.push_back(a);
+            
+//         }
+//         return res;
+        
+        
+//     }
+// };
+
 
 //
 
