e097148aa538d2a58eea3fabd2b7783110caf078
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-14 01:20:09 +0530

    Time: 31 ms (55.26%), Space: 19.5 MB (59.91%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) 
-    {
-        ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-        
-      unordered_map<int,int> um;
-        
-        int n= nums.size();
-        
+    int majorityElement(vector<int>& nums) {
+          int n=nums.size();
+        int vote =0,leader=0;
         for(int i=0;i<n;i++)
         {
-            um[nums[i]]++;
-            
-            if(um[nums[i]]>n/2)
-                return nums[i];
+            if(vote==0) leader=nums[i];
+            if(leader==nums[i]) vote++;
+            else vote--;
         }
-        
-        return -1;
+        return leader;
     }
 };
+
+//naive solution
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) 
+//     {
+//         ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+        
+//       unordered_map<int,int> um;
+        
+//         int n= nums.size();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             um[nums[i]]++;
+            
+//             if(um[nums[i]]>n/2)
+//                 return nums[i];
+//         }
+        
+//         return -1;
+//     }
+// };
