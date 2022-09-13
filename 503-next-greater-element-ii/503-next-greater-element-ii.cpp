b60ea57a91763e0c66ec5589ef816428206762ae
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-14 00:32:20 +0530

    Time: 27 ms (99.48%), Space: 23.8 MB (90.26%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -1,32 +1,56 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> l(n,-1),r(n,n);
-        stack<int> st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && nums[st.top()]<=nums[i]) st.pop();
-            if(!st.empty()){
-                r[i]=st.top();
+     vector<int> nextGreaterElements(vector<int>& nums) 
+     {
+        int n = nums.size();
+        vector<int> next(n, -1);
+        stack<int> s; // index stack
+        for (int i = 0; i < n * 2; i++) {
+            int num = nums[i % n]; 
+            while (!s.empty() && nums[s.top()] < num) {
+                next[s.top()] = num;
+                s.pop();
             }
-            st.push(i);
-        }
-        // for(int i=0;i<n;i++) cout<<r[i]<<" ";
-        // cout<<"\n";
-        vector<int> ans(n,-1);
-        // while(!st.empty()) st.pop();
-        vector<int> d;
-        for(int i=0;i<n;i++){
-            if(d.empty() || d.back()<nums[i]) d.push_back(nums[i]);
-            if(r[i]!=n) ans[i]=nums[r[i]];
-            else{
-                int pos=upper_bound(d.begin(),d.end(),nums[i])-d.begin();
-                if(pos<d.size()) ans[i]=d[pos];
-            }
-        }
-        return ans;
+            if (i < n) s.push(i);
+        }   
+        return next;
     }
+
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> l(n,-1),r(n,n);
+//         stack<int> st;
+//         for(int i=n-1;i>=0;i--){
+//             while(!st.empty() && nums[st.top()]<=nums[i]) st.pop();
+//             if(!st.empty()){
+//                 r[i]=st.top();
+//             }
+//             st.push(i);
+//         }
+//         // for(int i=0;i<n;i++) cout<<r[i]<<" ";
+//         // cout<<"\n";
+//         vector<int> ans(n,-1);
+//         // while(!st.empty()) st.pop();
+//         vector<int> d;
+//         for(int i=0;i<n;i++){
+//             if(d.empty() || d.back()<nums[i]) d.push_back(nums[i]);
+//             if(r[i]!=n) ans[i]=nums[r[i]];
+//             else{
+//                 int pos=upper_bound(d.begin(),d.end(),nums[i])-d.begin();
+//                 if(pos<d.size()) ans[i]=d[pos];
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
 // My solution 946ms
 
