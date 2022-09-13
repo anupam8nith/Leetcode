023fb06bda48b342ecfadce536f438fa5aa3aede
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-09-13 23:30:24 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (90.52%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -16,6 +16,48 @@ public:
 //
 // class Solution {
 // public:
+//     void f(vector<vector<int>> &ans,vector<int> &nums,vector<int>&cur,vector<int>&visited,/*set<vector<int>>&s,*/int n){
+//         if(cur.size()==n /*&& s.find(cur)==s.end()*/){
+//             ans.push_back(cur);
+//            // s.insert(cur);
+//             return;
+//         }
+//         for(int i=0;i<n;i++){
+//             if(visited[i]==0){
+//                 if(i!=0 && nums[i]==nums[i-1] && visited[i-1]==0){
+                    
+//                 }
+//                 else{
+//                     cur.push_back(nums[i]);
+//                 visited[i]=1;
+//                 f(ans,nums,cur,visited,/*s,*/n);
+//                 visited[i]=0;
+//                 cur.pop_back();
+//                 //f(ans,nums,cur,visited,s,n);
+                    
+//                 }
+                
+                
+                
+//             }
+//         }
+//     }
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         vector<vector<int>>ans;
+//         vector<int>cur;
+//         int n=nums.size();
+//         vector<int> visited(n,0);
+//         //set<vector<int>> s;
+//         sort(nums.begin(),nums.end());
+//         f(ans,nums,cur,visited,/*s,*/n);
+//         return ans;
+        
+//     }
+// };
+
+//
+// class Solution {
+// public:
 //     vector<vector<int>> permuteUnique(vector<int> &a) {
       
 //         sort(a.begin(),a.end());
