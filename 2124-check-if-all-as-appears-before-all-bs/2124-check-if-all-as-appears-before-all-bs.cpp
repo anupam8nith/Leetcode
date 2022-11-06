38a43dc20c1d4aa965234687c29346836eceed22
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-11-06 20:05:00 +0530

    Time: 4 ms (42.08%), Space: 6.2 MB (10.26%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool checkString(string s) 
+    {
+        int idxa=-1,idxb=-1,n=s.size(),counta=0,countb=0;
+        
+        int low=0,high=n-1;
+        
+        while(low<n || high>=0)
+        {
+            if(s[low]=='b' && countb==0 )
+            {   
+                countb++;
+                idxb=low;
+            }
+            if(s[high]=='a' && counta==0)
+            {   counta++;
+                idxa=high;
+            }
+           
+            if(idxa!=-1 && idxb!=-1)
+            {
+                if(idxa>idxb)
+                return false;
+                // else
+                // return true;
+            }
+            if(low<n)
+            low++;
+            if(high>=0)
+            high--;
+        }
+        
+        if(idxa==-1 || idxb==-1)
+            return true;
+        
+        if(idxa>idxb)
+        return false;
+        
+     return true;   
+    }
+};
