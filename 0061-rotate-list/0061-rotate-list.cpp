0287f4e278f588dede28c6207e97274ea631cc49
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-11-07 23:36:29 +0530

    Time: 26 ms (5.53%), Space: 11.8 MB (55.22%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -12,31 +12,88 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) 
     {
-        vector<int> v;
+        int count=0;
+        ListNode *p=head,*newhead=NULL,*end=NULL;
         
-        ListNode *p=head;
+        
         
         while(p!=NULL)
         {
-            v.push_back(p->val);
+            count++;
             p=p->next;
         }
-        
-        int n = size(v);
-        vector<int> v2(v);
-        
-        for(int i=0;i<n;i++)
-            v[(i+k)%n]=v2[i];
-        
-        p=head;
+        // cout<<count<<endl; //1.
+        if(count<=1 || k==0 || k%count==0)
+            return head;
         
         int i=0;
-        while(p!=NULL)
-        {
-            p->val = v[i++];
-            p=p->next;
-        }
+        p=head;
         
-        return head;
+        // cout<<p->val<<endl;
+        
+    
+        while(i!=count - k%count -1)
+        {
+        p=p->next;
+        i++;
+        }
+    // else
+    // {
+    //     while(i!=count - k%count -1)
+    //     {
+    //     p=p->next;
+    //     i++;
+    //     }
+    // }
+    
+        newhead=p->next;
+        
+        // cout<<newhead->val<<endl; //2.
+        end=p;
+        
+        while(p->next!=NULL && p!=NULL)
+            p=p->next;
+        
+        cout<<p->val<<endl;
+        
+        p->next=head;
+        end->next=NULL;
+        
+        return newhead;
+        
     }
 };
+
+
+// class Solution {
+// public:
+//     ListNode* rotateRight(ListNode* head, int k) 
+//     {
+//         vector<int> v;
+        
+//         ListNode *p=head;
+        
+//         while(p!=NULL)
+//         {
+//             v.push_back(p->val);
+//             p=p->next;
+//         }
+        
+//         int n = size(v);
+//         vector<int> v2(v);
+        
+//         for(int i=0;i<n;i++)
+//             v[(i+k)%n]=v2[i];
+        
+//         p=head;
+        
+//         int i=0;
+//         while(p!=NULL)
+//         {
+//             p->val = v[i++];
+//             p=p->next;
+//         }
+        
+//         return head;
+//     }
+// };
