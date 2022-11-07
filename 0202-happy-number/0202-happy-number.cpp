b8f7bd073ea82d73045b00bd36f9df9f8630cb5b
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-11-08 00:30:09 +0530

    Time: 4 ms (46.71%), Space: 6 MB (61.70%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-    set<int> s;
-    
-    bool isHappy(int n) 
-    {
-       if(n==1)
-        return true;
-        
-        int temp=0;
-        while(n!=0)
-        {
-            temp+=pow(n%10,2);
-            n/=10;
+    int getNextNum(int n) {
+        int nextNum = 0;
+        while(n) {
+            int d = (n % 10);
+            nextNum += d*d;
+            n /= 10;
         }
-        if(s.find(temp)!=s.end())
-            return false;
+        return nextNum;
+    }
+
+    bool isHappy(int n) {
+        //Floyd's Cycle Detection Algorithm
+        int slow = n;
+        int fast = n;
+        do {
+            slow = getNextNum(slow);
+            fast = getNextNum(getNextNum(fast));
+            if(fast == 1) return true;
+        } while (slow != fast);
         
-        s.insert(temp);
-        
-        return isHappy(temp);
-        
+        return false;
     }
 };
+// class Solution {
+// public:
+//     set<int> s;
+    
+//     bool isHappy(int n) 
+//     {
+//        if(n==1)
+//         return true;
+        
+//         int temp=0;
+//         while(n!=0)
+//         {
+//             temp+=pow(n%10,2);
+//             n/=10;
+//         }
+//         if(s.find(temp)!=s.end())
+//             return false;
+        
+//         s.insert(temp);
+        
+//         return isHappy(temp);
+        
+//     }
+// };
