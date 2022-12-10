09830e2b71fcd7f4f17a9a85c19aef8f861aedb0
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-12-11 00:54:13 +0530

    Time: 333 ms (31.49%), Space: 78.4 MB (44.00%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) 
     {
-        long long ans = INT_MAX, n = nums.size();
+        int ans = INT_MAX, n = nums.size();
         
         int idx=-1;
         
         
         // vector<int> left(n,-1), right(n,-1);
         
-        long long lsum=0;
+        
         
         // avg from left and from right
         
-        long long total = 0;
+        long long total = 0, lsum=0;
         
         for(auto x: nums)
             total+=x;
-        
-        cout<<total<<endl;
-        
+    
         for(int i=0;i<=n-2;i++)
         {
             lsum += nums[i];
