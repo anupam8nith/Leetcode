03206fe2b29982e95967bb62aa02b270ae36b8a5
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2022-12-19 19:14:55 +0530

    Time: 224 ms (85.30%), Space: 103 MB (49.43%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -39,3 +39,26 @@ public:
         
     }
 };
+
+//best solution
+
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         int n = temperatures.size();
+//         vector<int>answer(n,0);
+//         int maxTemp = 0;
+//         for(int currDay=n-1;currDay>=0;currDay--){
+//             int currTemp = temperatures[currDay];
+//             if(currTemp>=maxTemp){
+//                 maxTemp=currTemp;
+//                 continue;
+//             }
+//             int days = 1;
+//             while(currTemp>=temperatures[currDay+days])
+//                 days+=answer[currDay+days];
+//             answer[currDay]=days;
+//         }
+//         return answer;
+//     }
+// };
