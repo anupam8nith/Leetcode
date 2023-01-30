aa72950eb04624f2915bb22b39f89fc653975c3f
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-01-31 04:16:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (37.46%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -3,16 +3,30 @@ public:
     
     int dp[38];
     
+    int find(int n)
+    {
+        if( n<0 || n>37)
+            return 0;
+        
+       if(dp[n]!=0)
+            return dp[n];
+
+        if(n>=3)
+        dp[n]= dp[n-1]+dp[n-2]+dp[n-3];
+        
+        return dp[n] = find(n-1) + find(n-2) + find(n-3);;
+    }
+    
     int tribonacci(int n) 
     {
-        if(dp[n]!=0) return dp[n];
+        dp[1]=1;
+        dp[2]=1;
         
-        if(n==0) return 0;
-        if(n==1 || n==2) return 1;
+        if(dp[n]!=0)
+            return dp[n];
         
-        dp[n] =  tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        return find(n);
         
-        return dp[n];
         
     }
 };
