8c39664c88c7e326292aa3e946fa3b414c7b8205
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-02-16 01:20:47 +0530

    Time: 240 ms (76.45%), Space: 128.6 MB (7.86%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,36 +10,24 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head)
-    {
-        
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        
-        ListNode* p=head;
-        string s="";
-        
-        while(p!=NULL)
-        {
-            s+=(char)p->val;
-            p=p->next;
-            
+     bool isPalindrome(ListNode* head) {
+	
+        stack<ListNode*> nodeStack; // A stack is first in last out, traversing a list is quasi-first in first out
+		
+        ListNode* pHead = head;
+        while (pHead) {
+            nodeStack.push(pHead);
+            pHead = pHead->next;
         }
         
-        int n=size(s);
-        
-        n--;
-        
-        int i=0;
-        
-        while(i<n)
-            if(s[i++]!=s[n--])
-                return false;
-        
-        return true;
-        
-        
-        
+        while (head) {
+            if (head->val != nodeStack.top()->val) { // We are going forwards on the linked list and backwards via a stack.
+                return false;  // If the values differ, well it isn't a palindrome.
+            }
+            nodeStack.pop(); // Similar to head->next but in reverse.
+            head = head->next;
+        }
+        return true; // If we get to the end of the linked list and the values in the stack didn't differ from those
+		// of the linked list, then it is a palindrome.
     }
 };
