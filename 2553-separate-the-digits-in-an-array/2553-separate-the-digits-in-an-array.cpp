bc2ec8367767e6c7dd94c0eb11a7a8daaabf42ed
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-03-19 11:50:58 +0530

    Time: 7 ms (87.51%), Space: 10 MB (84.60%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -1,32 +1,50 @@
 class Solution {
 public:
-    void solve(vector<int> & nums, vector<int> &answer,int &n)
-    {
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans ;
         
-        for(int i=0;i<n;i++)
-        {
-            vector<int> temp;
-            while(nums[i]!=0)
-            {
-                int rem = nums[i]%10;
-                nums[i]/=10;
-                temp.push_back(rem);
-            }
-            reverse(temp.begin(),temp.end());
-            for(auto it: temp)answer.push_back(it);
+        for( auto &it : nums ){
+            
+            string tmp = to_string(it);
+            
+            for( auto &s : tmp ) ans.push_back(s-'0');
+            
         }
-    }
-    
-    
-    vector<int> separateDigits(vector<int>& nums) 
-    {
-        int n =size(nums);
         
-        vector<int> answer;
-        
-        solve(nums,answer,n);
-        
-        return answer;
-        
+        return ans ;
     }
 };
+
+
+// class Solution {
+// public:
+//     void solve(vector<int> & nums, vector<int> &answer,int &n)
+//     {
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             vector<int> temp;
+//             while(nums[i]!=0)
+//             {
+//                 int rem = nums[i]%10;
+//                 nums[i]/=10;
+//                 temp.push_back(rem);
+//             }
+//             reverse(temp.begin(),temp.end());
+//             for(auto it: temp)answer.push_back(it);
+//         }
+//     }
+    
+    
+//     vector<int> separateDigits(vector<int>& nums) 
+//     {
+//         int n =size(nums);
+        
+//         vector<int> answer;
+        
+//         solve(nums,answer,n);
+        
+//         return answer;
+        
+//     }
+// };
