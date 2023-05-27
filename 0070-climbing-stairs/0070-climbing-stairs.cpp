e3cd44ec4bf1c3d49b34f91dbc06f8b3fc53ae2d
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-05-27 19:46:32 +0530

    Time: 3 ms (37.09%), Space: 5.9 MB (96.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,10 +4,12 @@ public:
    int dp[46];
     int climbStairs(int n) 
     {   
-        if(dp[n]!=0) return dp[n];
-  
-        if(n==1 || n==2) return n;
-        dp[n]=climbStairs(n-1)+climbStairs(n-2);
+        dp[0]=1;dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]+=dp[i-1]+dp[i-2];
+        }
+        
         return dp[n];
     }
 };
