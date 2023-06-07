cdbccccf4a9e330ab30f5e11822b61e28c92e9cf
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-06-07 15:45:49 +0530

    Time: 8 ms (74.32%), Space: 10.1 MB (23.16%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& v) 
     {
+        //slope of line joining first 2 points should be equal to slope of all the pairs of points.
+        
         for(int i=1;i<(v.size()-1);i++) 
-            if( (v[0][1]-v[1][1])*(v[i][0]-v[i+1][0]) != (v[i][1]-v[i+1][1])*(v[0][0]-v[1][0]) ) 
+            if( (v[0][1]-v[1][1])*(v[i][0]-v[i+1][0]) != (v[i][1]-v[i+1][1])*(v[0][0]-v[1][0]) )
                 return false;
             
         return true;
