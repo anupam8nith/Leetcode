e8e9df66522fa016a92dcb0cdb0e47c3e519ebea
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-06-15 11:40:05 +0530

    Time: 214 ms (35.68%), Space: 107.2 MB (84.30%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,41 +11,55 @@
  */
 class Solution {
 public:
-    int maxLevelSum(TreeNode* root) 
-    {
-        int level =1, ans=1, mxtill=INT_MIN;
-        
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        while(!q.empty())
-        {
-            int sz = q.size();
-            int sum=0;
-            for(int i=0;i<sz;i++)
-            {
-                TreeNode* node = q.front();
-                q.pop();
-                
-                sum+=node->val;
-                
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-                
-            }
-    
-            
-            if(mxtill<sum)
-            {
-                mxtill=sum;
-                ans=level;
-            }
-            
-            level++;
-            
-            cout<<mxtill<<endl;
+  int maxLevelSum(TreeNode* root) {
+    // Initialize the level, answer, and maximum sum.
+    int level = 1;
+    int ans = 1;
+    int max_till = INT_MIN;
+
+    // Create a queue to store the nodes of the current level.
+    queue<TreeNode*> q;
+    q.push(root);
+
+    // While the queue is not empty, do the following:
+    while (!q.empty()) {
+      // Get the size of the current level.
+      int sz = q.size();
+
+      // Initialize the sum of the current level.
+      int sum = 0;
+
+      // Iterate over all the nodes of the current level.
+      for (int i = 0; i < sz; i++) {
+        // Get the node at the front of the queue.
+        TreeNode* node = q.front();
+        q.pop();
+
+        // Add the value of the node to the sum.
+        sum += node->val;
+
+        // If the node has a left child, add it to the queue.
+        if (node->left) {
+          q.push(node->left);
         }
-        
-        return ans;
+
+        // If the node has a right child, add it to the queue.
+        if (node->right) {
+          q.push(node->right);
+        }
+      }
+
+      // If the sum of the current level is greater than the maximum sum so far, update the maximum sum and the answer.
+      if (sum > max_till) {
+        max_till = sum;
+        ans = level;
+      }
+
+      // Increment the level.
+      level++;
     }
+
+    // Return the answer.
+    return ans;
+  }
 };
