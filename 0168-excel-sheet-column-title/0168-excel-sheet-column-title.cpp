7afb90b9cf269e51060672a8b5d9010804fb4457
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-08-22 17:16:41 +0530

    Time: 2 ms (49.78%), Space: 6 MB (37.93%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,36 +1,56 @@
 class Solution {
 public:
-    string convertToTitle(int n) 
-    {
+    string convertToTitle(int c) {
+  string s;
+  while(c>0)
+{
+    c--;
+    int d=c%26;
+
+    s+='A'+d;
+    c=c/26;
     
-        string ans ="", str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        if(n<=26)
-        {
-            ans +=str[n-1];
-            return ans;
-        }
-        
-        
-        while(n>26)
-        {
-            int rem = n%26;
-            n/=26;
-            
-            if(rem!=0)
-            ans+= str[rem-1];
-            else
-            {
-             ans+=str[25];
-             n--;
-            }
-            
-        }
-        
-        ans+=str[n-1];
-        
-        reverse(ans.begin(),ans.end());
-        
-        return ans;
-        
+    }
+    reverse(s.begin(),s.end());
+    return s;
+
     }
 };
+
+
+// class Solution {
+// public:
+//     string convertToTitle(int n) 
+//     {
+    
+//         string ans ="", str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//         if(n<=26)
+//         {
+//             ans +=str[n-1];
+//             return ans;
+//         }
+        
+        
+//         while(n>26)
+//         {
+//             int rem = n%26;
+//             n/=26;
+            
+//             if(rem!=0)
+//             ans+= str[rem-1];
+//             else
+//             {
+//              ans+=str[25];
+//              n--;
+//             }
+            
+//         }
+        
+//         ans+=str[n-1];
+        
+//         reverse(ans.begin(),ans.end());
+        
+//         return ans;
+        
+//     }
+// };
