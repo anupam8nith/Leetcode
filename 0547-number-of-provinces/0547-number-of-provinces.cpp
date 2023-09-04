b766ebe68ff78724a82343fc3b706f348749207f
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-09-04 16:14:06 +0530

    Time: 25 ms (61.12%), Space: 16.5 MB (41.01%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     
-    void bfs(vector<vector<int>> &adj, vector<int>&vis, int i)
+    void dfs(vector<vector<int>> &adj, vector<int>&vis, int node)
     {
-        queue<int> q;
-        q.push(i);
-        vis[i]=1;
+        vis[node]=1;
         
-        while(!q.empty())
-        {
-            int node = q.front();
-            q.pop();
-            
-            for(auto it: adj[node])if(!vis[it])
-            {q.push(it);vis[it]=1;}
-        }
+        for(auto it: adj[node])
+            if(!vis[it])
+                dfs(adj,vis,it);
         
         return;
     }
@@ -33,7 +26,7 @@ public:
         for(int i=0;i<n;i++)
             if(!vis[i])
             {
-                bfs(adj,vis,i);
+                dfs(adj,vis,i);
                 ans++;
             }
                 
