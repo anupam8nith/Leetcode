c705f99514db082772ec02238c0fe4344eb1d622
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-09-13 22:58:07 +0530

    Time: 18 ms (53.51%), Space: 18 MB (29.28%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& r) {
+        int n = r.size();
+        vector<int> v(n, 1);
+        
+        for (int i = 1; i < n; i++) {
+            if (r[i] > r[i - 1]) {
+                v[i] = v[i - 1] + 1;
+            }
+        }
+        
+        int ans = v[n - 1];
+        
+        for (int i = n - 2; i >= 0; i--) {
+            if (r[i] > r[i + 1] && v[i] <= v[i + 1]) {
+                v[i] = v[i + 1] + 1;
+            }
+            ans += v[i];
+        }
+        
+        return ans;
+    }
+};
