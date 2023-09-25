cb6342034111ef9161b04fe58c4cce76dd8ca35b
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-09-25 18:44:21 +0530

    Time: 4 ms (48.11%), Space: 6.9 MB (25.22%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     char findTheDifference(string s, string t) 
     {
-        unordered_map<char,int> ums,umt;
-        for(auto ch: s)ums[ch]++;
-        for(auto ch: t)umt[ch]++;
-        
-        for(auto ch: t)if(umt[ch]!=ums[ch]) return ch;
-        
-        
-        
-        return' ';
+        char c = 0;
+        for(char cs : s) c ^= cs;
+        for(char ct : t) c ^= ct;
+        return c;
         
     }
 };
