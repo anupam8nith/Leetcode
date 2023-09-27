1a2f5d67f77a30004a6b262eaa4c684d732e6748
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-09-27 15:55:45 +0530

    Time: 125 ms (90.83%), Space: 65.8 MB (52.08%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) 
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        
+        vector<vector<int>> adj(n+1);
+        
+       for (auto & t : trust) adj[t[0]].push_back(t[1]);
+        
+        for(int i=1;i<=n;i++)
+            if(adj[i].empty())
+            {
+                int trustcount=0;
+                for(int j=1;j<=n;j++)
+                {
+                    if(j!=i && find(adj[j].begin(),adj[j].end(),i)!=adj[j].end())
+                        trustcount++;
+                }
+                
+                if(trustcount==n-1)
+                    return i;
+            }
+        
+        
+        return -1;
+    }
+};
