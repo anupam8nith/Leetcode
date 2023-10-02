c94323bb35f2d080c814da044aa8bb70cb030a16
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-10-02 11:18:59 +0530

    Time: 11 ms (69.25%), Space: 12.4 MB (5.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,8 +4,6 @@ public:
     {
         int n = nums.size();
         
-        vector<int> ans;
-        
         unordered_map<int,int> um;
         for(int i=0;i<n;i++) um[nums[i]]=i;
     
@@ -13,15 +11,11 @@ public:
         for(int i=0;i<n;i++)
         {
             if(um.find(target-nums[i])!=um.end() && um[target-nums[i]]!=i)
-            {
-                ans.push_back(i);
-                ans.push_back(um[target-nums[i]]);
-                return ans;
-            }
+            return {um[target-nums[i]],i};
             
         }
         
         
-        return ans;
+        return {};
     }
 };
