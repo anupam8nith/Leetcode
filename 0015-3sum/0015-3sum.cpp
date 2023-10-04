c47609c408f94ab0bbc441ff85612acf717c3104
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-10-04 23:22:55 +0530

    Time: 1119 ms (19.83%), Space: 192 MB (10.75%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
         int n =nums.size();
         sort(nums.begin(),nums.end());
         
