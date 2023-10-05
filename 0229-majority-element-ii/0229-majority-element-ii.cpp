53da5a06229b5fbf685a2de1ac6efd1f20c04dc6
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-10-05 16:46:56 +0530

    Time: 7 ms (87.90%), Space: 16.6 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) 
     {
+        ios_base::sync_with_stdio(false);cin.tie(0);
+        
         int n = nums.size();vector<int> ans;unordered_map<int,int> um;
         
         for(auto it: nums)um[it]++;
