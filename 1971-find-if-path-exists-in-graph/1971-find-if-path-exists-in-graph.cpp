9cd941da51de75260084beb606298ac90b97837d
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-10-05 18:02:59 +0530

    Time: 662 ms (25.79%), Space: 275.6 MB (5.30%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     void dfs(unordered_map<int,vector<int>> &adj,vector<bool> &visited,int start, int end)
     {
-        visited[start]=true;
+        visited[start]=true; //just visit the node and mark visited
         
         for(auto neighbor: adj[start])
-        {
             if(visited[neighbor]==false)dfs(adj,visited,neighbor,end);
-        }
         
         return;
     }
@@ -17,16 +15,16 @@ public:
         if(source==destination || n==1) return true;
         
         unordered_map<int,vector<int>> adj;
-        vector<bool> visited(n+1,false);
+        vector<bool> visited(n+1,false); //visited bool array
         
-        for(int i=0;i<edges.size();i++)
+        for(int i=0;i<edges.size();i++) //prepare an adjacency list
         {
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
         dfs(adj,visited,source,destination);
         
-        return visited[destination];
+        return visited[destination]; //return true if we have been there
         
     }
 };
