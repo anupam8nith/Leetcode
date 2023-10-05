1b82f53365513fe7e2c90c9c879538dee6953496
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-10-05 23:35:13 +0530

    Time: 11 ms (99.17%), Space: 10.7 MB (7.61%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -3,20 +3,14 @@ class Solution
 public:
     int threeSumClosest(vector<int>& nums, int target) 
     {
-        // ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        int n =nums.size();
-        if(nums.size() == 3) return nums[0] + nums[1] + nums[2];
+        int n =nums.size(); if(nums.size() == 3) return nums[0] + nums[1] + nums[2];
+        
         sort(nums.begin(), nums.end());
         
         int res = nums[0] + nums[1] + nums[2];
-        
         if(target <= res)return res;
         
         if(nums[n-1] + nums[n-2] + nums[n-3] <= target) return nums[n-1] + nums[n-2] + nums[n-3];
-        
-        vector<pair<int,int>> ans;
         
         for(int i=0;i<n;i++)
         {
