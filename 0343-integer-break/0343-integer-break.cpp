6e83ad8fff80a2c6ede1aa6c92cb79a41040062d
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-10-06 12:26:26 +0530

    Time: 2 ms (49.96%), Space: 6.7 MB (17.22%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -2,6 +2,10 @@ class Solution {
 public:
     int integerBreak(int n) 
     {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        
         if(n==2) return 1;if(n==3) return 2; int twos=0,threes=n/3;
     
         if(n>3 && n%3==0) return ((int)pow(3,threes));
