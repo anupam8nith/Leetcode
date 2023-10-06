1a07bf23e89634d287c907a4468ab59036d302d1
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-10-06 12:27:25 +0530

    Time: 2 ms (49.96%), Space: 6.7 MB (11.85%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -19,3 +19,21 @@ public:
         return (int)(pow(2,twos)*pow(3,threes));
     }
 };
+
+// class Solution {
+// public:
+//     int solve(int n , vector<int>&dp){
+//         if(n<=3)return n;
+//         if(dp[n]!= -1)return dp[n];
+//         int maxi = INT_MIN;
+//         for(int i=2;i<n;i++){
+//             maxi = max(maxi, i* solve(n-i, dp));
+//         }
+//         return dp[n] = maxi;
+//     }
+//     int integerBreak(int n) {
+//         if(n<=3)return n-1;
+//         vector<int>dp(n+1, -1);
+//         return solve(n, dp);
+//     }
+// };
