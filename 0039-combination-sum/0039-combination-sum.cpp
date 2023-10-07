76419ebd330ce797dea6141f00476dcce1af5faf
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-10-07 14:44:17 +0530

    Time: 39 ms (23.29%), Space: 18.5 MB (27.05%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+void findall(vector<vector<int>>&ans, vector<int> &candidates, vector<int> currVec, int idx, int sum, int target)
+    {
+        if (sum == target)
+        {
+            ans.push_back(currVec);
+            return;
+        }
+        if (idx > candidates.size() || sum > target) return;
+
+        for(int i=idx;i<candidates.size();i++)
+        {
+                currVec.push_back(candidates[i]);
+                findall(ans, candidates, currVec, i, sum + candidates[i], target);
+                currVec.pop_back();
+        }   
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target)
+    {
+        int n = candidates.size(); vector<vector<int>>ans;
+        sort(candidates.begin(), candidates.end());
+
+        long long sum = 0;
+        vector<int> currVec;
+
+        findall(ans, candidates, currVec, 0, sum, target);
+
+        return ans;
+    }
+};
