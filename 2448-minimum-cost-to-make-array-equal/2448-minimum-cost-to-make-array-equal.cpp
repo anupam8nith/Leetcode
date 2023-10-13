22f2bafc6295284494333c39fb00c45c40c01f19
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-10-13 11:30:26 +0530

    Time: 73 ms (55.95%), Space: 43.7 MB (27.19%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,34 +1,44 @@
-#define ll long long
 class Solution {
 public:
-    ll find(int mid , vector<int>& nums, vector<int>& cost){
-        ll ans = 0;
-
-        for(int i = 0 ; i < nums.size() ; i++){
-            ans += 1LL*(1LL*abs(nums[i]-mid)*1LL*cost[i]);
-        }
-        return ans;
-    }
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        ll left = *min_element(nums.begin(), nums.end());
-        ll right = *max_element(nums.begin(),nums.end()); 
-        ll min_cost = LLONG_MAX;
+        int n=nums.size();
+        vector<pair<int, int>> arr;
+        for(int i=0; i<n; i++) {
+            arr.push_back({nums[i], cost[i]});
+        }
+        sort(begin(arr), end(arr));
 
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            ll p = find(mid,nums,cost);
-            ll q = find(mid+1,nums,cost);
-            if(p<q){
-                right = mid - 1;
-                min_cost = min(min_cost,p);
-            }
-            else{
-                left = mid + 2;
-                min_cost = min(min_cost , q);
-            }
+        //first calculate cost for nums[0]
+        // what calculate    it's  make all otheer elements equal to it..
+        long long sum = 0;
+        for(int i=1; i<n; i++) {
+            long long gap  = arr[i].first - arr[0].first;
+            sum += gap*arr[i].second;
+        }
 
+        //calculate prefix sum array for costs 
+        // inorder to apply the sum in some range
+        vector<long long> prefix(n);
+        prefix[0] = arr[0].second;
+        for(int i=1; i<n; i++) {
+            prefix[i] = arr[i].second + prefix[i-1];
+        }
+
+        //treat every other index as breaking point and find min
+        long long min_cost = sum;
+        for(int i=1; i<n; i++) {
+            
+            //previous elements are being already made equal to it
+            int gap(arr[i].first - arr[i-1].first);
+            
+            //increase the sum to the front
+            sum += prefix[i-1]*gap;
+            
+            //as previously we added the change to sum    we need to remove it now... NOte that your array is in asc order
+            sum -= (prefix.back() - prefix[i-1])*gap;
+
+            min_cost = min(sum, min_cost);
         }
         return min_cost;
-       
     }
 };
