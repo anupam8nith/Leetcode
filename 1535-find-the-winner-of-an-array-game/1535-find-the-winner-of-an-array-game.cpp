9a8a1fe0a7fd15381c413c715eabbed526beb193
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-06 21:32:11 +0000

    Time: 83 ms (69.88%), Space: 67.1 MB (19.79%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) 
+    {
+        int n = arr.size();
+        if(k>=n-1) return *max_element(arr.begin(),arr.end());
+        
+        queue<int>q;
+        
+        for(auto it: arr)q.push(it);
+        
+        
+        int wcount=0, winner=q.front();q.pop();
+        while(wcount!=k)
+        {
+           int ele2=q.front();q.pop();
+            
+            if(winner>ele2){q.push(ele2);wcount++;}
+            else{wcount=0;q.push(winner);winner = ele2;wcount++;}
+            
+            if(wcount==k) return winner;
+        }
+        
+        return winner;
+    }
+};
