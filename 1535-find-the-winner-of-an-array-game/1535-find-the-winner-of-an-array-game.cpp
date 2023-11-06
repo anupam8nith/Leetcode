ae602f2ee5a558374bc090a8ff85853ed01a45dc
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-06 21:32:31 +0000

    Time: 103 ms (11.07%), Space: 67.2 MB (19.07%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int getWinner(vector<int>& arr, int k) 
     {
+        cin.tie(0);
+        
         int n = arr.size();
         if(k>=n-1) return *max_element(arr.begin(),arr.end());
         
