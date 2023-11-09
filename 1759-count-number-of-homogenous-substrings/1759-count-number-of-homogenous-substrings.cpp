e26a82904355572053883bb1c73e59e9346c88e4
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-09 07:56:39 +0000

    Time: 14 ms (99.13%), Space: 12.1 MB (33.48%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -2,19 +2,44 @@ class Solution {
 public:
     int countHomogenous(string s) 
     {
-        int left = 0;
-        long long res = 0;
-        
-        for (int right = 0; right < s.length(); right++) 
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        long long a = 0;
+        int j =0;
+        long long r= 0;
+        for(int i =0 ; i<s.size() ; i++)
         {
-            if (s[left] == s[right]) res += right - left + 1;
-            else 
+            for(j= i ; j<s.size() ; j++)
             {
-                res += 1;
-                left = right;
+                if(s[j]!=s[i]) break;
+                r++;
             }
+            i =j-1;
+            a = a + r*(r+1)/(2);
+            r= 0;
         }
-
-        return (int) (res % (1000000007));        
+        return a%1000000007;
     }
 };
+
+// class Solution {
+// public:
+//     int countHomogenous(string s) 
+//     {
+//         int left = 0;
+//         long long res = 0;
+        
+//         for (int right = 0; right < s.length(); right++) 
+//         {
+//             if (s[left] == s[right]) res += right - left + 1;
+//             else 
+//             {
+//                 res += 1;
+//                 left = right;
+//             }
+//         }
+
+//         return (int) (res % (1000000007));        
+//     }
+// };
