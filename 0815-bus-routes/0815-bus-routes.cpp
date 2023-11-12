cab1825260f0408d75e81d2c9dd90f5aa4b64fea
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-12 20:45:09 +0000

    Time: 2229 ms (5.15%), Space: 47.2 MB (95.11%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -1,42 +1,58 @@
-class Solution {
+class Solution 
+{
 public:
-    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) 
+    {
         if (source == target) return 0;
-
+        int bus_mx = INT_MIN;
+        
+        for (auto it : routes)
+        {
+            int x = it.size();
+            bus_mx = max(bus_mx, it[x - 1]);
+        }
+        
+        // bus_no -> routes it is contained in
         map<int, vector<int>> mp;
-
-        for (int i = 0; i < routes.size(); ++i) {
-            for (const int stop : routes[i]) {
-                mp[stop].push_back(i);
+        
+        for (int bus = 0; bus <= bus_mx; bus++)
+        {
+            for (int i = 0; i < routes.size(); i++)
+            {
+                if (binary_search(routes[i].begin(), routes[i].end(), bus))
+                    mp[bus].push_back(i);
             }
         }
-
-        queue<int> q;
-        q.push(source);
-
+        
+        // find ans
+        queue<int> q; 
+        q.push(source); 
         int ans = 0;
         vector<bool> visited(routes.size(), false);
-
-        while (!q.empty()) {
+        
+        while (!q.empty())
+        {
             int size = q.size();
-            while (size--) {
-                int curr = q.front();
+            while (size--)
+            {
+                int curr = q.front(); 
                 q.pop();
-
-                for (const int route : mp[curr]) {
+                
+                for (auto route : mp[curr])
+                {
                     if (visited[route]) continue;
-
                     visited[route] = true;
-
-                    for (const int stop : routes[route]) {
-                        if (stop == target) return ans + 1;
-                        q.push(stop);
-                    }
+                    
+                    // for each route no, check whether it has target
+                    if (binary_search(routes[route].begin(), routes[route].end(), target)) 
+                        return ans + 1;
+                    else 
+                        for (auto stop : routes[route])
+                            q.push(stop);
                 }
             }
-            ++ans;
-        }
-
+            ans++;
+        } 
         return -1;
     }
 };
