295127a25adba7f3092a0d12d0c803a0fa1a2102
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-13 20:22:43 +0000

    Time: 41 ms (56.50%), Space: 17.5 MB (8.56%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -6,16 +6,16 @@ public:
         
         for(int i=0;i<s.size();i++) 
         {
-            char c= s[i];
-            bool isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-            bool isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+            auto c= s[i];
+            auto isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+            auto isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
             if(isLowercaseVowel || isUppercaseVowel) {pos.push_back(i); vowels+=c;}
         }
         
         sort(vowels.begin(),vowels.end());
         
         int j=0;
-        for(int i: pos)ans[i]=vowels[j++];
+        for(auto i: pos)ans[i]=vowels[j++];
         
         return ans;
     }
