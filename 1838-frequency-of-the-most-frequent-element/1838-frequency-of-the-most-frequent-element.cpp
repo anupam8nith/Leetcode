f8d0a0e50bb5b6d66013841b813cf7a2b3d4fd0e
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-18 19:45:15 +0000

    Time: 295 ms (5.01%), Space: 108 MB (5.34%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k) 
-    {
-         int maxf=0;
-        long currsum=0;
-        sort(nums.begin(),nums.end());
-        for(int left=0,right=0;right<nums.size();++right){
-            currsum+=nums[right];
-            while(currsum+k< static_cast<long>(nums[right])*(right-left+1)){
-                currsum-=nums[left];
-                left++;
-            }
-            maxf=max(maxf,right-left+1);
+    int maxFrequency(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        vector<long long> prefixSum(n + 1);
+
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
         }
-        return maxf;
-        
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int lo = 0, hi = i;
+
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+
+                long long total = (long long)nums[i] * (i - mid + 1);
+                long long sum = prefixSum[i + 1] - prefixSum[mid];
+
+                if (total - sum <= k) {
+                    hi = mid;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+
+            ans = max(ans, i - lo + 1);
+        }
+
+        return ans;
     }
 };
