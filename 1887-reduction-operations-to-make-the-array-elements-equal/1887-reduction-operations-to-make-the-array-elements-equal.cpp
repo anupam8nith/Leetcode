d85e8521daec4ec48607ca04edb9decf5c91569a
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-19 06:08:28 +0000

    Time: 119 ms (99.08%), Space: 83.1 MB (68.40%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,22 +1,20 @@
-class Solution {
+class Solution 
+{
 public:
     int reductionOperations(vector<int>& nums) 
     {
-        map<int, int> mp;
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
         
-        for (int num : nums) mp[num]++;
-        
-        int ans = 0, mini = mp.begin()->first, maxi = prev(mp.end())->first, cnt = mp[mini];
-        
-        for (int i = mini + 1; i <= maxi; i++) 
-        {
-            if (mp.find(i) != mp.end())
-            {
-                ans += nums.size() - cnt;
-                cnt += mp[i];
-            }
-        }
-        
-        return ans;
+    sort(nums.begin(),nums.end());
+
+    int res = 0;
+    int c = 0;
+    for(int i=1; i<nums.size(); i++)
+    {
+        if(nums[i] != nums[i-1])
+        {c++;}
+        res += c;
     }
+    return res;
+}
 };
