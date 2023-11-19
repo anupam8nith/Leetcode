6ee8f24cba74584a96699875c50a458c0306a75d
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-19 06:07:09 +0000

    Time: 477 ms (6.14%), Space: 124.6 MB (35.58%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int reductionOperations(vector<int>& nums) 
     {
-        int ans = 0;map<int, int> mp;
+        map<int, int> mp;
         
-        for (int num : nums)mp[num]++;
+        for (int num : nums) mp[num]++;
         
-        while (mp.size() > 1) 
+        int ans = 0, mini = mp.begin()->first, maxi = prev(mp.end())->first, cnt = mp[mini];
+        
+        for (int i = mini + 1; i <= maxi; i++) 
         {
-            auto it = mp.rbegin();
-            int temp = it->first, freq = it->second;
-            ans += freq;
-            mp.erase(temp);
-            
-            if (!mp.empty()) 
+            if (mp.find(i) != mp.end())
             {
-                it = mp.rbegin(); // Update the iterator
-                it->second += freq; 
+                ans += nums.size() - cnt;
+                cnt += mp[i];
             }
         }
         
