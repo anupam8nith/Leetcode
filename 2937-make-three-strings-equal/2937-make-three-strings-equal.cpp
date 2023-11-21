425e18194cd4abe25504ed42a7056b4fd601fc65
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-21 18:23:24 +0000

    Time: 13 ms (8.39%), Space: 7.3 MB (47.40%) - LeetHub

--- 2937-make-three-strings-equal/2937-make-three-strings-equal.cpp
+++ 2937-make-three-strings-equal/2937-make-three-strings-equal.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int findMinimumOperations(string s1, string s2, string s3) 
     {
-        if(s1==s2 && s2==s3) return 0;
         if(s1[0]!=s2[0] || s1[0]!=s3[0]) return -1;
+        if(s1==s2 && s1==s3) return 0;
         
         string s; int n1=s1.size(),n2 = s2.size(), n3= s3.size();
         
-        if(n1<=n2 && n1 <=n3)s=s1;
-        else if(n2<=n1 && n2 <=n3) s=s2;
-        else if(n3<=n1 && n3<=n2) s=s3;
+        if(n1<=n2 && n1 <=n3)s=s1;else if(n2<=n1 && n2 <=n3) s=s2;else s=s3;
         
         int i=0;
         while(s[i]==s1[i] && s1[i]==s2[i] && s2[i]==s3[i])i++;
-        // cout<<i<<endl;
         
         return n1+n2+n3-3*(i);
     }
