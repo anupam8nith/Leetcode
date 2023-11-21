39163d95c27fb430604ba07347d66e9e2a8528eb
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-21 18:26:30 +0000

    Time: 4 ms (85.02%), Space: 7.2 MB (88.58%) - LeetHub

--- 2937-make-three-strings-equal/2937-make-three-strings-equal.cpp
+++ 2937-make-three-strings-equal/2937-make-three-strings-equal.cpp
@@ -5,12 +5,10 @@ public:
         if(s1[0]!=s2[0] || s1[0]!=s3[0]) return -1;
         if(s1==s2 && s1==s3) return 0;
         
-        string s; int n1=s1.size(),n2 = s2.size(), n3= s3.size();
-        
-        if(n1<=n2 && n1 <=n3)s=s1;else if(n2<=n1 && n2 <=n3) s=s2;else s=s3;
+        int n1=s1.size(),n2 = s2.size(), n3= s3.size();
         
         int i=0;
-        while(s[i]==s1[i] && s1[i]==s2[i] && s2[i]==s3[i])i++;
+        while(s1[i]==s2[i] && s2[i]==s3[i])i++;
         
         return n1+n2+n3-3*(i);
     }
