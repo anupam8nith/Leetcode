e0c645fb7afd81c99ce09cf942e57b2fd0b0375e
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-22 08:30:26 +0000

    Time: 231 ms (34.40%), Space: 108.8 MB (11.96%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) 
     {
-        map<int,vector<int>> um;
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
         
-        for(int i=0;i<nums.size();i++)for(int j=0;j<nums[i].size();j++)um[i+j].push_back(nums[i][j]);
+        map<int,vector<int>> um; vector<int> diag;
         
-        vector<int> diag;
+        for(int i=0;i<nums.size();i++) for(int j=0;j<nums[i].size();j++) um[i+j].push_back(nums[i][j]);
         
-        for(auto it: um)for(auto ptr = it.second.rbegin(); ptr != it.second.rend(); ++ptr)diag.push_back(*ptr);
+        for(auto it: um) for(auto ptr = it.second.rbegin();ptr!=it.second.rend();++ptr) diag.push_back(*ptr);
         
         return diag;
         
