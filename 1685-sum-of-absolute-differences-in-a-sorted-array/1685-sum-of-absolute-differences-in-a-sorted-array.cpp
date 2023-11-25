e69e15fc2ef09b52821cbf75f60797018afc60d0
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-25 14:39:29 +0000

    Time: 94 ms (53.51%), Space: 86.2 MB (42.97%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) 
-    {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        int n = nums.size();
-        vector<int> right(n), left(n), ans(n);
+    {   
+        int n = nums.size(), total=0; 
+        vector<int> right(n), ans(n);
         
         
-        right[0]=nums[0];left[n-1]=nums[n-1];
-        
+        right[0]=nums[0];
         for(int i=1;i<n;i++)right[i]+=right[i-1]+nums[i];
         
-        for(int i=n-2;i>=0;i--)left[i]+=left[i+1]+nums[i];
+        total = accumulate(nums.begin(),nums.end(),0);
+        
         
         
         for(int i=0;i<n;i++)
-        ans[i]= abs(right[i]-(i+1)*(nums[i])) + abs(left[i]-((n-i-1)*nums[i])) - nums[i];
+        ans[i]= abs(right[i]-(i+1)*(nums[i])) + abs((total-right[i])-((n-i-1)*nums[i]));
         
         
         return ans;
