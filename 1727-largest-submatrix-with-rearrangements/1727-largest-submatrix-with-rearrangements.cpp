14bc1b5d68a8914045fdcaa747ef159e7753c833
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-26 09:19:37 +0000

    Time: 154 ms (66.48%), Space: 74.8 MB (57.95%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -3,7 +3,7 @@ public:
     int largestSubmatrix(vector<vector<int>>& matrix) 
     {
         ios::sync_with_stdio(0);
-        cin.tie(0);
+        
         
         
         int m = matrix.size(), n = matrix[0].size();int ans = 0;
