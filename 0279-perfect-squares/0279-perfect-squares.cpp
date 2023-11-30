8d02e888416486f1bc9df326ef0f63437a8c118a
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-30 21:04:01 +0000

    Time: 0 ms (100.00%), Space: 6.7 MB (93.91%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,29 +1,17 @@
-class Solution {
+class Solution 
+{
 public:
-    int numSquaresReal(int n) {
-    while (n % 4 == 0)
-        n /= 4;
-    if (n % 8 == 7)
-        return 4;
-    int a = -1, b = sqrt(n);
-    n -= b * b;
-    b += b + 1;
-    while (a <= b) {
-        if (n < 0)
-            n += b -= 2;
-        else if (n > 0)
-            n -= a += 2;
-        else
-            return a < 0 ? 1 : 2;
-    }
-    return 3;
-}
-    int numSquares(int n) 
-    {
-        
-        int sum = 0;
-    for (int i=0; i<10000; i++)
-        sum += numSquaresReal(n);
-    return sum / 10000;
+   int numSquares(int n) 
+   {
+        static vector<int> dp {0};
+
+        while (dp.size() <= n) 
+        {
+            int m = dp.size(), squares = INT_MAX;
+            for (int i=1; i*i<=m; ++i)
+                squares = min(squares, dp[m-i*i] + 1);
+            dp.push_back(squares);
+        }
+        return dp[n];
     }
 };
