df607e26c03362cca76cc3bd791c48329f7b35a1
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-11-30 20:31:45 +0000

    Time: 110 ms (99.36%), Space: 33.9 MB (49.79%) - LeetHub

--- 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.cpp
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.cpp
@@ -1,9 +1,10 @@
+unordered_set<int> st;
+vector<int> primes;
 class Solution {
 public:
-    vector<vector<int>> findPrimePairs(int n) 
+    
+    void getPrimes(int n)
     {
-        ios_base::sync_with_stdio(false);
-        
         bitset<1000000>isNotPrime;
 
         isNotPrime[0] = 1;
@@ -15,14 +16,24 @@ public:
                 for (int j = i * i; j < n; j += i) isNotPrime[j] = 1;
         }
         
-        vector<vector<int>> ans;
+        for(int i=2;i<n;i++)if(isNotPrime[i]==0) 
+        { 
+              st.insert(i); 
+              primes.push_back(i); 
+         }
         
-        for(int i=2;i<n;i++)
-        {
-            if(isNotPrime[i])continue;
-            else if(isNotPrime[n-i]==false)
-            {ans.push_back({i,n-i});isNotPrime[n-i]=true;}
-        }
+        return;
+    }
+    
+    vector<vector<int>> findPrimePairs(int n) 
+    {
+        ios_base::sync_with_stdio(false);
+        if(st.size()==0)getPrimes(1000000);
+        
+        vector<vector<int>> ans;
+        for(int i = 0; i < primes.size() && primes[i] <= n/2; ++i)
+            if(st.find(n - primes[i]) != st.end()) 
+            {  ans.push_back({primes[i], n - primes[i]});  }
         
         return ans;
     }
