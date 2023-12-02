7f50627adb8b18cfcf47efb418cdee9d20bca0fd
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-02 21:32:49 +0000

    Time: 118 ms (19.70%), Space: 21.2 MB (9.85%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,94 @@
+class Solution {
+public:
+    vector<int> primes;
+   
+    int distinctPrimeFactors(vector<int>& nums) 
+    {
+        set<int>s;
+        if(primes.size()==0)primes= SieveOfAtkin(1e4);
+        
+        for(auto& n: nums)
+        {
+            for(int i=0;i<primes.size();i++)
+            {
+                if(primes[i]>n)break;
+                else if(n%primes[i]==0)s.insert(primes[i]);
+            }
+        }
+        
+        return s.size();
+    }
+    
+    
+vector<int> SieveOfAtkin(int limit) 
+{
+        vector<bool> isPrime(limit + 1, false);
+        vector<int> primes;
+
+        // Mark small primes
+        if (limit >= 2) primes.push_back(2);
+        if (limit >= 3) primes.push_back(3);
+
+        // Initialize sieve arrays
+        vector<bool> sieve(limit + 1, false);
+
+        int sqrtLimit = static_cast<int>(sqrt(limit));
+        for (int x = 1; x <= sqrtLimit; x++) {
+            for (int y = 1; y <= sqrtLimit; y++) {
+                int n = (4 * x * x) + (y * y);
+                if (n <= limit && (n % 12 == 1 || n % 12 == 5))
+                    sieve[n] = !sieve[n];
+
+                n = (3 * x * x) + (y * y);
+                if (n <= limit && (n % 12 == 7))
+                    sieve[n] = !sieve[n];
+
+                n = (3 * x * x) - (y * y);
+                if (x > y && n <= limit && (n % 12 == 11))
+                    sieve[n] = !sieve[n];
+            }
+        }
+
+        for (int n = 5; n <= sqrtLimit; n++) {
+            if (sieve[n]) {
+                int sqrN = n * n;
+                for (int k = sqrN; k <= limit; k += sqrN) {
+                    sieve[k] = false;
+                }
+            }
+        }
+
+        // Collect primes
+        for (int i = 5; i <= limit; i++) {
+            if (sieve[i])
+                primes.push_back(i);
+        }
+
+        return primes;
+}
+};
+
+// class Solution {
+// public:
+//     bool isPrime(int x)
+//     {
+//         if(x==2|| x==3) return true;
+        
+//         for(int i=2;i*i<=x;i++)if(x%i==0)return false;
+        
+//         return true;
+//     }
+//     int distinctPrimeFactors(vector<int>& nums) 
+//     {
+        
+//         set<int> s;
+        
+//         for(auto& n: nums)
+//         {
+//             for(int i=2;i*i<=n;i++)
+//                 if(n%i==0)if(isPrime(i))
+//         }
+        
+//         return s.size();
+//     }
+// };
