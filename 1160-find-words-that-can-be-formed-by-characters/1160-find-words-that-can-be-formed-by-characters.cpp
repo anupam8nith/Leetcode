e59a25431e64414e4fe2c9a33238a2de6013505c
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-02 07:53:58 +0000

    Time: 155 ms (28.33%), Space: 49.3 MB (35.07%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) 
+    {
+        unordered_map<char,int> um; int ans=0;
+        for(char& ch: chars)um[ch]++;
+        
+        for(string& str:words)
+        {
+            unordered_map<char,int> temp;
+            for(char& ch: str)temp[ch]++;
+            
+            bool flag=true;
+            for(auto &it: temp)if(um[it.first]<it.second){flag=false;break;}
+            
+            if(flag)ans+=str.size();
+        }
+        
+        return ans;
+    }
+};
