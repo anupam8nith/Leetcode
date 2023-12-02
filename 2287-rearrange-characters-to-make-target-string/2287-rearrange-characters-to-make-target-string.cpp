4db8327865fe25a367e8cf5e260b22ee939b1eda
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-02 08:28:10 +0000

    Time: 0 ms (100.00%), Space: 6.4 MB (94.37%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int rearrangeCharacters(string s, string target) 
     {
-        int count[26]{}; int ans=0;
+        int count1[26]{}; int count2[26]{};
+        for(char& ch: s)count1[ch-'a']++;
         
-        for(char& ch : s) ++count[ch - 'a'];
+        for(char& ch: target)count2[ch-'a']++;
         
-        while(true)
-        {
-            for(char& ch : target){if(count[ch-'a']>=1)--count[ch-'a'];else return ans;}
-            
-            ans++;
-        }
+        for(int i=0; i<26; i++)if(count2[i])count1[i]/=count2[i];
+        
+        int ans = INT_MAX;
+        for(char& ch: target)ans = min(ans, count1[ch-'a']);
         
         return ans;
     }
