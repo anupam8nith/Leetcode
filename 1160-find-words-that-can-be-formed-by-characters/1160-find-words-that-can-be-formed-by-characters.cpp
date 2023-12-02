129f5ffcffbce8fbbe267aa69d22b4767fbe56af
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-02 08:07:35 +0000

    Time: 23 ms (100.00%), Space: 15.8 MB (87.44%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -3,6 +3,8 @@ class Solution
 public:
     int countCharacters(vector<string>& words, string chars) 
     {
+        ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+        
         int count[26]{}; int ans = 0;
         
         for(char& c : chars) ++count[c - 'a'];
