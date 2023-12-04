8da017840d18cf8c4222e220a12cb5d62ba1fadc
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-04 15:12:30 +0000

    Time: 0 ms (100.00%), Space: 7.2 MB (7.83%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     string largestGoodInteger(string num) 
     {
+        ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+        
         string ans="";
         
         for(int i=1;i<num.size()-1;i++) if(num[i]==num[i-1] && num[i]==num[i+1])
