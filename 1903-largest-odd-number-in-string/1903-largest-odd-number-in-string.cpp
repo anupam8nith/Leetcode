173ed74669fa59253ae712587bfaca69c7777dfc
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-07 02:42:34 +0000

    Time: 24 ms (86.63%), Space: 15.3 MB (37.80%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -4,12 +4,12 @@ public:
     {
         ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
         
-        int n = num.size(); string ans="";
+        int n = num.size();
         
         for(int i=n-1;i>=0;i--)
-            if(((num[i]-'0'))&1){ans=max(ans,num.substr(0,i+1));break;}
+            if(((num[i]-'0'))&1) return num.substr(0,i+1);
         
-        return ans;
+        return "";
         
     }
 };
