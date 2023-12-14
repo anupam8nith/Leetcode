7752281c3e6965e2b1f4c621b45dcef65a2e4192
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-14 11:59:08 +0000

    Time: 7 ms (86.85%), Space: 11.1 MB (30.24%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -2,30 +2,13 @@ class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) \
     {
-        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-        
         int m = matrix.size(),n=matrix[0].size();
         
-        if(m==n)
-        {
-            for(int i=0;i<matrix.size();i++)
-                for(int j=0;j<i;j++)
-                    swap(matrix[i][j],matrix[j][i]);
-            
-            return matrix;
-        }
-        else
-        {
-            vector<vector<int>> transposed(matrix[0].size(), vector<int>(matrix.size(), 0));
+        vector<vector<int>> transposed(n, vector<int>(m, 0));
             for(int i=0;i<n;i++)
                 for(int j=0;j<m;j++)
                     transposed[i][j]=matrix[j][i];
             
-            return transposed;
-        }
-            
-        
-        return matrix;
-        
+        return transposed;
     }
 };
