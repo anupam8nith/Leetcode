f12ade126f0a506b2d3727799084451559ead2c9
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-15 20:19:26 +0000

    Time: 95 ms (33.25%), Space: 18 MB (14.50%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
@@ -3,8 +3,8 @@ from collections import Counter
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
         n = len(arr)//4
-        elementCount= Counter(arr)
+        hashmap= Counter(arr)
         
-        for num,freq in elementCount.items():
+        for num,freq in hashmap.items():
             if freq>n:
                 return num
