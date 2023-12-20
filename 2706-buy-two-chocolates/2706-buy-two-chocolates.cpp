31d6c289391dee9e8eb0568181ce437338b31f3a
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-20 12:12:19 +0530

    Time: 8 ms (81.59%), Space: 47 MB (58.30%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -2,8 +2,19 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) 
     {
-        sort(prices.begin(),prices.end());
+        int mn1=INT_MAX, mn2=INT_MAX;
         
-        return (prices[0]+prices[1]) <= money ? (money-(prices[0]+prices[1])) : money;
+        for(auto& it: prices)
+        {
+            if(mn1>=it){mn2=mn1;mn1=it;continue;}
+            
+            // if(mn1==it){mn2=mn1;continue;}
+            
+            if(it<mn2 && it>mn1)mn2=it;;
+        }
+        
+        cout<<mn1<<" "<<mn2<<endl;
+        
+        return mn1+mn2<=money ? money-(mn1+mn2) : money;
     }
 };
