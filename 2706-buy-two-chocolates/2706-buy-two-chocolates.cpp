d2537a54578036120b2b261020226127251dfcab
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-20 12:13:45 +0530

    Time: 3 ms (98.75%), Space: 47.1 MB (13.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) 
     {
+        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
         int mn1=INT_MAX, mn2=INT_MAX;
         
         for(auto& it: prices)
