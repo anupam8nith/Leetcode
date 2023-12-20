23affa27e8c68383f54767509f9eb9af698aaebe
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-20 12:04:25 +0530

    Time: 12 ms (59.66%), Space: 47.1 MB (13.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -4,6 +4,6 @@ public:
     {
         sort(prices.begin(),prices.end());
         
-        return (prices[0]+prices[1]) <= money ? -1*(prices[0]+prices[1]-money) : money;
+        return (prices[0]+prices[1]) <= money ? (money-(prices[0]+prices[1])) : money;
     }
 };
