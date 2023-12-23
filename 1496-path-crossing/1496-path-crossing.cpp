9316acd406732a7048ccfcabd333f8cee336f4bb
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-24 03:35:06 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (93.89%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
+    struct pairHash {
+        size_t operator()(const pair<int, int>& p) const {
+            return hash<int>()(p.first) ^ hash<int>()(p.second);
+        }
+    };
+    
     bool isPathCrossing(string path) {
-        set<pair<int, int>> loc = {{0, 0}};  // Use set for efficient lookups
+        unordered_set<pair<int, int>, pairHash> loc = {{0, 0}}; // Use custom hash function
         pair<int, int> position = {0, 0};
 
         for (char ch : path) {
-            position = { position.first + (ch == 'E') - (ch == 'W'),
-                         position.second + (ch == 'N') - (ch == 'S') };
-            if (!loc.insert(position).second) return true;  // Check for insertion success
+            if (ch == 'N') position.second++;
+            else if (ch == 'S') position.second--;
+            else if (ch == 'E') position.first++;
+            else if (ch == 'W') position.first--;
+
+            if (!loc.insert(position).second) return true;
         }
 
         return false;
