b7df91576670fd5dc7278871b8662c056a890b1e
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-24 02:54:34 +0530

    Time: 189 ms (66.89%), Space: 119.3 MB (23.16%) - LeetHub

---
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) 
+    {
+        int rows= grid.size(), col = grid[0].size();
+        
+        vector<vector<int>> diff(rows, vector<int>(col));
+        
+        vector<int> onesCol(col),onesRow(rows), zeroCol(col), zeroRow(rows);
+        
+        //count zeroes and ones in rows
+        for(int i=0;i<rows;i++)
+        {
+            int count1=0,count0=0;
+            
+            for(int j=0;j<col;j++)
+            {
+                if(grid[i][j]==0)count0++;
+                else if(grid[i][j]==1)count1++;
+            }
+            
+            onesRow[i]=count1;zeroRow[i]=count0;
+        }
+        
+        for(int j=0;j<col;j++)
+        {
+            int count1=0,count0=0;
+            
+            for(int i=0;i<rows;i++)
+            {
+                if(grid[i][j]==0)count0++;
+                else if(grid[i][j]==1)count1++;
+            }
+            
+            onesCol[j]=count1;zeroCol[j]=count0;
+        }
+        
+        for(int i=0;i<rows;i++)
+            for(int j=0;j<col;j++)
+                diff[i][j]=onesRow[i]+onesCol[j]-zeroRow[i]-zeroCol[j];
+        
+        return diff;
+        
+    }
+};
