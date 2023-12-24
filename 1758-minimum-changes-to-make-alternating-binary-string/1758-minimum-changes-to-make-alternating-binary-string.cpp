ef9ff7739446fd91252633cb8f31e953294c1eaa
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-24 10:00:02 +0530

    Time: 2 ms (78.64%), Space: 7.2 MB (86.65%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,17 +1,22 @@
-class Solution 
-{
+class Solution {
 public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
     
     int minOperations(string s) 
     {
         int n = s.size(),ans=0;
-
+        
         for(int i=1;i<n;i++)
         {
             if( (s[i-1]=='0' && s[i]=='1') || (s[i-1]=='1' && s[i]=='0') )continue;
             
             if(s[i-1]=='0'){s[i]='1';ans++;continue;}
-            if(s[i-1]=='1'){s[i]='0';ans++;continue;}
+            else {s[i]='0';ans++;continue;}
             
         }
         
