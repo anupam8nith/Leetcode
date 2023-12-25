06f8427bd29da7e418d76546805acaf750f7c4f4
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-26 02:13:23 +0530

    Time: 3 ms (47.76%), Space: 6.3 MB (98.47%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,17 +1,16 @@
-class Solution {
+class Solution { // 0 ms, faster than 100.00%
 public:
-    int numDecodings(string& s) 
-    {
-        int n = s.size();
-        vector<int> dp(n+1, 0);
-        dp[n] = 1;
-        for (int i = n - 1; i >= 0; --i) 
-        {
+    int numDecodings(const string& s) {
+        int n = s.size(), dp = 0, dp1 = 1, dp2 = 0;
+        for (int i = n - 1; i >= 0; --i) {
             if (s[i] != '0') // Single digit
-                dp[i] += dp[i+1];
-            if (i+1 < s.size() && ((s[i] == '1' && s[i+1] <= '9') || (s[i] == '2' && s[i+1] <= '6'))   ) // Two digits
-                dp[i] += dp[i+2];
+                dp += dp1;
+            if (i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) // Two digits
+                dp += dp2;
+            dp2 = dp1;
+            dp1 = dp;
+            dp = 0;
         }
-        return dp[0];
+        return dp1;
     }
 };
