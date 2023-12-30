57c82b28a9229dd0f2019423d1fb9db58e538457
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2023-12-30 15:35:48 +0530

    Time: 36 ms (5.06%), Space: 12 MB (81.71%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0;i<n;i++)
            for(int j=0;j<words[i].size();j++)freq[words[i][j]-'a']++;
        
-        for(auto& it: freq)if(it%n!=0) return false;
+        for(int i=0;i<26;i++)if(freq[i]%n!=0) return false;
         
         return true;
     }
