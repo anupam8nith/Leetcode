e57c2543b85851e13a41dcc9b715e26e3f78084c
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-02 02:22:18 +0530

    Time: 49 ms (5.16%), Space: 18.1 MB (60.83%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int candy(vector<int>& r) {
-        int n = r.size();
-        vector<int> v(n, 1);
-        
-        for (int i = 1; i < n; i++) {
-            if (r[i] > r[i - 1]) {
-                v[i] = v[i - 1] + 1;
-            }
-        }
-        
-        int ans = 0;
-        
-        for (int i = n - 2; i >= 0; i--) {
-            if (r[i] > r[i + 1] && v[i] <= v[i + 1]) {
-                v[i] = v[i + 1] + 1;
-            }
-        }
-        
-        for(auto it: v)ans+=it;
-        
-        return ans;
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        if(n <= 1) return n;
+
+        vector<int> num(n, 1);
+        for (int i = 1; i < n; i++)
+            if (ratings[i] > ratings[i-1])
+                num[i] = num[i-1] + 1;
+
+        for (int i= n-1; i>0 ;i--)
+            if (ratings[i-1] > ratings[i])
+                num[i-1] = max(num[i]+1, num[i-1]);
+
+        int result=0;
+        for (int i = 0; i < n; i++)
+            result += num[i];
+
+        return result;
     }
 };
