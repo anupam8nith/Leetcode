12331db25a5f273dce00ec6246253ffaf8a0666f
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-01 15:14:57 +0530

    Time: 29 ms (15.10%), Space: 17.9 MB (36.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) 
     {
-        std::ios::sync_with_stdio(false);
-        cin.tie(NULL);
+        std::ios::sync_with_stdio(false);cin.tie(NULL);
         
         int count=0, i=0,j=0;
         sort(g.begin(),g.end()); sort(s.begin(),s.end());
