bb8d6dde51efd25cf3a65d8497363d52d63851ee
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-01 15:13:30 +0530

    Time: 62 ms (5.95%), Space: 17.8 MB (75.56%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,16 +5,18 @@ public:
 //         std::ios::sync_with_stdio(false);
 //         cin.tie(NULL);
         
-        multiset<int> ms;
-        
-        for(int& it: s)ms.insert(it); int ans=0; 
-        
-        for(int& target: g)
+        int count=0, i=0,j=0;
+        sort(g.begin(),g.end()); sort(s.begin(),s.end());
+
+       while(i<g.size() && j<s.size())
         {
-            auto ptr = ms.lower_bound(target);
-            if(ptr!=ms.end()){ans++;ms.erase(ptr);}
-        }
-        
-        return ans;
+                if(s[j]>=g[i])
+                {
+                    count++; i++;
+                    if(count==s.size()) return count;
+                }
+                j++;
+        }        
+        return count;
     }
 };
