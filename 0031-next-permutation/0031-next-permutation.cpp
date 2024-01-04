bd2a2b87dd04a7ba586f31ed7211d82174de759f
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-04 15:51:52 +0530

    Time: 4 ms (55.82%), Space: 12.4 MB (92.54%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,33 +5,33 @@
 // Swap nums[k] and nums[l].
 // Reverse the sub-array nums[k + 1:].
 
-class Solution {
-public:
-    void nextPermutation(vector<int>& nums) {
-    	int n = nums.size(), k, l;
-    	for (k = n - 2; k >= 0; k--) {
-            if (nums[k] < nums[k + 1]) {
-                break;
-            }
-        }
-    	if (k < 0) {
-    	    reverse(nums.begin(), nums.end());
-    	} else {
-    	    for (l = n - 1; l > k; l--) {
-                if (nums[l] > nums[k]) {
-                    break;
-                }
-            } 
-    	    swap(nums[k], nums[l]);
-    	    reverse(nums.begin() + k + 1, nums.end());
-        }
-    }
-};
-
 // class Solution {
 // public:
-//     void nextPermutation(vector<int>& nums) 
-//     {
-//         next_permutation(nums.begin(),nums.end());   
+//     void nextPermutation(vector<int>& nums) {
+//     	int n = nums.size(), k, l;
+//     	for (k = n - 2; k >= 0; k--) {
+//             if (nums[k] < nums[k + 1]) {
+//                 break;
+//             }
+//         }
+//     	if (k < 0) {
+//     	    reverse(nums.begin(), nums.end());
+//     	} else {
+//     	    for (l = n - 1; l > k; l--) {
+//                 if (nums[l] > nums[k]) {
+//                     break;
+//                 }
+//             } 
+//     	    swap(nums[k], nums[l]);
+//     	    reverse(nums.begin() + k + 1, nums.end());
+//         }
 //     }
 // };
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) 
+    {
+        next_permutation(nums.begin(),nums.end());   
+    }
+};
