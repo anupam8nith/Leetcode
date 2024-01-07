7298a1f92c3e397075efa71e808b1544ebbc5d92
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-07 20:24:08 +0530

    Time: 54 ms (96.11%), Space: 14.8 MB (97.22%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,25 +1,31 @@
+using ll = long;
+const uint max_n = 1000;
+uint dp[max_n][max_n];
+
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        int ans=0, n=nums.size();
-        
-        vector<map<int,int>> maps(n);
-
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                long cd=(long)nums[i]-(long)nums[j];
-                
-                if(cd<=INT_MIN || cd>=INT_MAX) continue;
-                
-                int count1=maps[j][(int)cd];
-                int count2=maps[i][(int)cd];
-
-                ans+=count1;
-                maps[i][(int)cd]=count1+count2+1;
+    int numberOfArithmeticSlices(vector<int>& a) {
+        uint n = a.size();
+        uint res = 0;
+        unordered_map<int, vector<uint>> ai = {};
+        for (uint i = 0; i < n; i++) ai[a[i]].push_back(i);
+        for (uint i = 1; i < n; i++) {
+            for (uint j = 0; j < i; j++) {
+                dp[i][j] = 0;
+                auto p = 2l * a[j] - a[i];
+                if (p < INT_MIN || p > INT_MAX)
+                    continue;
+                auto ii = ai.find(p);
+                if (ii != ai.end()) {
+                    for (auto k : ai[p]) {
+                        if (k >= j)
+                            break;
+                        dp[i][j] += dp[j][k] + 1;
+                    }
+                }
+                res += dp[i][j];
             }
         }
-        return ans;
+        return res;
     }
 };
