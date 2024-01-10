a57bf06f857127560a09ecb7cc3afb3e49e4e6dd
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-10 19:14:19 +0530

    Time: 569 ms (21.97%), Space: 318.3 MB (5.18%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     Solution()
     {
-        ios_base::sync_with_stdio(false);cin.tie(0);
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
     }
     
     unordered_map<TreeNode*, vector<TreeNode*>> adj;  
