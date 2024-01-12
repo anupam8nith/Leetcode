bca4dc9f8eed81910310b43d36e1d8dc3c7c644b
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-12 14:16:59 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (46.92%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
+    
+    int count(string str)
+    {
+        int ans=0;
+        for(int i=0;i<str.size();i++)
+        {
+            char ch = tolower(str[i]);
+            if(ch == 'a'|| ch== 'e' || ch=='i' || ch=='o' || ch=='u')
+                ans++;
+        }
+        
+    return ans;
+    }
     bool halvesAreAlike(string s) 
     {
-        int n = size(s), a=0,b=0;
-        
-        for(int i=0;i<n/2;i++)
-        {
-            s[i]=tolower(s[i]);
-            if(s[i]==97 || s[i]==101 || s[i]==105 || s[i]==111 || s[i]==117)
-                a++;
-        }
-        
-        for(int i=n/2;i<n;i++)
-        {
-             s[i]=tolower(s[i]);
-            if(s[i]==97 || s[i]==101 || s[i]==105 || s[i]==111 || s[i]==117)
-                b++;
-        }
-        
-        if(a==b)
-        return true;
+        if(count(s.substr(0,s.size()/2)) == count(s.substr(s.size()/2,s.size()/2)))
+            return true;
         
         return false;
+        
     }
 };
