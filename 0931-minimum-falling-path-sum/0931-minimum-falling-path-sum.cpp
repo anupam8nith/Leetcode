193e1cf5e714f6f3904b1cb7f4d1954c239de9e8
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-19 16:23:15 +0530

    Time: 14 ms (44.96%), Space: 12.4 MB (5.13%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    bool isValid(int x, int y, int r, int c)
+    bool isValid(int x, int y, int n)
     {
-        if(x<r && y<c && x>=0 && y>=0) return true;
+        if(x<n && y<n && x>=0 && y>=0) return true;
         
         return false;
     }
     
     int minFallingPathSum(vector<vector<int>>& matrix) 
     {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        int r = matrix.size(), c= matrix[0].size(); 
+        int n = matrix.size();
 
-        for(int i=1;i<r;i++)
-            for(int j=0;j<c;j++)
+        for(int i=1;i<n;i++)
+            for(int j=0;j<n;j++)
             {
                 int temp = INT_MAX;
                 
-                if(isValid(i-1,j-1,r,c)) temp= min(temp,matrix[i-1][j-1]);
-                if(isValid(i-1,j,r,c)) temp= min(temp,matrix[i-1][j]);
-                if(isValid(i-1,j+1,r,c)) temp= min(temp,matrix[i-1][j+1]);
+                if(isValid(i-1,j-1,n)) temp= min(temp,matrix[i-1][j-1]);
+                if(isValid(i-1,j,n)) temp= min(temp,matrix[i-1][j]);
+                if(isValid(i-1,j+1,n)) temp= min(temp,matrix[i-1][j+1]);
                 
                 matrix[i][j]+=temp;      
             }
         
-        return *min_element(matrix[r-1].begin(),matrix[r-1].end());
+        return *min_element(matrix[n-1].begin(),matrix[n-1].end());
     }
 };
