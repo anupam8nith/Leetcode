969f45b642491b62f7bf37941f4f58bdff4e242c
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-20 09:54:55 +0530

    Time: 98 ms (74.85%), Space: 17.1 MB (8.93%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,33 @@
+class Solution 
+{
+public:
+    bool isValid(int x, int y, int n)
+    {
+        if(x<n && y<n && x>=0 && y>=0) return true;
+        
+        return false;
+    }
+    
+    int minFallingPathSum(vector<vector<int>>& matrix) 
+    {
+        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+        int n = matrix.size();
+
+        for(int i=1;i<n;i++)
+            for(int j=0;j<n;j++)
+            {
+                int temp = INT_MAX;
+                
+                for(int k=0;k<n;k++)
+                {
+                    if(k==j)continue;
+                    
+                    if(isValid(i-1,k,n)) temp= min(temp,matrix[i-1][k]);
+                }
+                
+                matrix[i][j]+=temp;
+            }
+        
+        return *min_element(matrix[n-1].begin(),matrix[n-1].end());
+    }
+};
