3848c042218fb29c67215cd09858333382081c08
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-25 00:19:22 +0530

    Time: 234 ms (96.25%), Space: 178.5 MB (96.51%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,28 +1,21 @@
-class Solution 
-{
-    int result;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
 public:
-    bool isPowerOfTwo(int n){
-        return n == 0 ? true : !(n&(n-1));
-    }
-
-    void dfs(TreeNode* root, int value){
-        if(root == 0)return;
-
-        value ^= 1 << root->val;
-
-        if(root->left == root->right){
-            result += static_cast<int>(isPowerOfTwo(value));
-        }
-        else {
-            dfs(root->left, value);
-            dfs(root->right, value);
-        }
-    }
-    int pseudoPalindromicPaths (TreeNode* root) {
-        result = 0;
-        dfs(root, 0);
-        root->left = root->right = 0;
-        return result;
+    int pseudoPalindromicPaths(TreeNode* root, int count = 0) {
+        if (!root) return 0;
+        count ^= 1 << (root->val - 1);
+        int res = pseudoPalindromicPaths(root->left, count) + pseudoPalindromicPaths(root->right, count);
+        if (root->left == root->right && (count & (count - 1)) == 0) res++;
+        return res;
     }
 };
