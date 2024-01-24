a3267e18c4d64020bd1f8ef327a57ad82685e710
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-25 00:08:33 +0530

    Time: 151 ms (100.00%), Space: 127.4 MB (100.00%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,87 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+class Solution 
+{
+    int result;
 public:
-    
-    int count = 0;
-//     bool canbepalindrome(unordered_map<int,int> &um)
-//     {
-//         int oddcount=0;
-        
-//         for(auto freq: um)
-//         {
-//             if(freq.second % 2 == 1)
-//                 oddcount++;
-            
-// //             if(oddcount>1)
-// //                 return false;
-//         }
-        
-//         // if(oddcount==0)
-//         //     return true;
-//         // else if(oddcount==1)
-//         //     return true;
-//         // else
-//         //     return true;
-        
-//         if(oddcount <= 1){
-//             return true;
-//         }else{
-//             return false;
-//         }
-//     }
-    
-    void getstring(TreeNode * root,unordered_map<int,int> &um)
-    {
-        if(!root) return;
-        
-         um[root->val]++;
-        if(root->left==NULL && root->right==NULL)
-        {
-            int odd = 0;
-            for(auto el : um){
-                if(el.second %2 == 1)
-                    odd++;
-            }
-            
-            if(odd <= 1)
-              count++;
-
-        }
-        
-       
-      
-        
-        getstring(root->left,um);
-        getstring(root->right,um);
-                     um[root->val]--;
-
-
+    bool isPowerOfTwo(int n){
+        return n == 0 ? true : !(n&(n-1));
     }
-    
-    
-    int pseudoPalindromicPaths (TreeNode* root) 
-    {
-        // ios_base::sync_with_stdio(false);
-        // cin.tie(NULL);
-        
-    
-        
-        if(root->left==NULL and root->right==NULL) return 1;
-        
-        unordered_map<int,int> um;
-        
-        getstring(root,um);
-        
-        return count;
+
+    void dfs(TreeNode* root, int value){
+        if(root == 0)return;
+
+        value ^= 1 << root->val;
+
+        if(root->left == root->right){
+            result += static_cast<int>(isPowerOfTwo(value));
+        }
+        else {
+            dfs(root->left, value);
+            dfs(root->right, value);
+        }
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        result = 0;
+        dfs(root, 0);
+        root->left = root->right = 0;
+        return result;
     }
 };
