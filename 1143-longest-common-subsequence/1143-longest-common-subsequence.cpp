f070f06c9bd751a361a1ece913cbd0c701f62f84
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-01-26 06:28:47 +0530

    Time: 10 ms (99.78%), Space: 9.7 MB (86.85%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -6,7 +6,13 @@ public:
 
     for(int i=0;i<a.size();i++)
         for(int j=0;j<b.size();j++)
-            dp[i+1][j + 1] = a[i] == b[j] ? dp[i][j] + 1 : max(dp[i][j + 1], dp[i+1][j]);
+        {
+            if(a[i] == b[j])
+            dp[i+1][j + 1] = dp[i][j] + 1;
+        
+            else
+            dp[i+1][j+1] = max(dp[i][j + 1], dp[i+1][j]);
+        }
         
     return dp[a.size()][b.size()];
 }
