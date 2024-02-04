d613222edb5d68e0570b36f15e760f78c176de26
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-02-04 16:47:49 +0530

    Time: 3 ms (30.43%), Space: 7.2 MB (96.95%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    int countDigits(int n) {
+    int countDigits(int n) 
+    {
         int d = 0;
-        while (n > 0) {
+        while (n > 0) 
+        {
             d++;
             n /= 10;
         }
         return d;
     }
 
-    vector<int> sequentialDigits(int low, int high) {
+    vector<int> sequentialDigits(int low, int high) 
+    {
         vector<int> ans;
         int startDigits = countDigits(low);
         int endDigits = countDigits(high);
 
-        for (int digits = startDigits; digits <= endDigits; ++digits) {
-            for (int i = 1; i <= 10 - digits; ++i) {
+        for (int digits = startDigits; digits <= endDigits; ++digits) 
+        {
+            for (int i = 1; i <= 10 - digits; ++i) 
+            {
                 int num = 0;
-                for (int j = i; j < i + digits; ++j) {
-                    num = num * 10 + j;
-                }
-                if (num >= low && num <= high) {
-                    ans.push_back(num);
-                }
+                for (int j = i; j < i + digits; ++j) num = num * 10 + j;
+                
+                if (num >= low && num <= high) ans.push_back(num);
             }
         }
-
         return ans;
     }
 };
