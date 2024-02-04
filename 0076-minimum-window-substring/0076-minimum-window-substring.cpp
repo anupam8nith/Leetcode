80d7518dba901cf05125327061cedd8cc6209291
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-02-04 16:11:42 +0530

    Time: 24 ms (29.22%), Space: 10.5 MB (9.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,40 +1,39 @@
-class Solution 
-{
-private:
-    bool containsAll(const unordered_map<char, int>& smap, const unordered_map<char, int>& tmap) 
-    {
-        for (const auto& p : tmap) 
-        {
-            if (smap.count(p.first) == 0 || smap.at(p.first) < p.second) return false;
-        }
-        return true;
-    }
-    
+class Solution {
 public:
-    string minWindow(string s, string t) 
-    {
+    string minWindow(string s, string t) {
         int m = s.size(), n = t.size();
-        
-        if(m<n) return "";
-        
+
+        if (m < n) return "";
+
         unordered_map<char, int> tmap, smap;
 
         for (char ch : t) tmap[ch]++;
 
         int i = 0, j = 0, minStart = 0, minLen = INT_MAX;
 
-        while (j < m) 
-        {
-            smap[s[j]]++;
+        int counter = n;
 
-            while (i <= j && containsAll(smap, tmap)) 
-            {
-                if (j - i + 1 < minLen) 
-                {
-                    minStart = i;minLen = j - i + 1;
+        while (j < m) {
+            char ch = s[j];
+            smap[ch]++;
+
+            if (tmap.count(ch) && smap[ch] <= tmap[ch]) {
+                counter--;
+            }
+
+            while (counter == 0) {
+                if (j - i + 1 < minLen) {
+                    minStart = i;
+                    minLen = j - i + 1;
                 }
 
-                smap[s[i]]--;
+                char ch = s[i];
+                smap[ch]--;
+
+                if (tmap.count(ch) && smap[ch] < tmap[ch]) {
+                    counter++;
+                }
+
                 i++;
             }
             j++;
@@ -42,5 +41,4 @@ public:
 
         return minLen == INT_MAX ? "" : s.substr(minStart, minLen);
     }
-
 };
