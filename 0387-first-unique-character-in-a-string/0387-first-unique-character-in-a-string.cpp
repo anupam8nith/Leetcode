60dc0c22f986581effb264e0e459e23e92c307b3
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-02-05 22:29:55 +0530

    Time: 9 ms (99.72%), Space: 11.9 MB (24.32%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int firstUniqChar(string s) 
     {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+        
         vector<int> freq(26,0);
         
         for(auto& ch: s)freq[ch-'a']++;
