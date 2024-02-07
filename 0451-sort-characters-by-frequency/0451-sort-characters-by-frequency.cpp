44a922221f25bed582a30042f6b7cdded2ed2818
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-02-07 20:39:02 +0530

    Time: 13 ms (42.09%), Space: 11 MB (11.70%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,23 +1,26 @@
 #pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
-    string frequencySort(string& s) {
+    string frequencySort(string s) 
+    {
+        unordered_map<char,int> freq;
+        
+        for(auto ch: s)freq[ch]++;
+        
         using int_char=pair<int, char>;
-        int freq[75];//'0'=48 'z'=122=>'z'-'0'+1=75        
-        for(char c : s)
-            freq[c-'0']++;
-
-        vector<int_char> freq_c;
-        for(char c='0'; c<='z'; c++){
-            if (freq[c-'0']==0) continue;
-            freq_c.emplace_back(freq[c-'0'], c);
-        }
-        sort(freq_c.begin(), freq_c.end(), greater<int_char>());
-        string ans;
-        for (auto& [n, c]: freq_c){
-            ans+=string(n, c);
-        }
-//        cout<<ans<<endl;
-        return ans;       
+        vector<int_char>v;
+        
+        for(auto ele: freq)
+                v.push_back({ele.second,ele.first});
+        
+        sort(v.begin(),v.end(),greater<int_char>());
+        
+        string ans ="";
+        
+        for(auto [n,c] :v)
+           ans+= string(n,c);
+        
+        return ans;
+        
     }
 };
