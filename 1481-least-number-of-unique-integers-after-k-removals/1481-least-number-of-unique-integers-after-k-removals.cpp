0dce358e7cf0de398feadc4aa7f380ead7c9668a
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-02-16 14:38:17 +0530

    Time: 131 ms (64.77%), Space: 66 MB (60.95%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,27 +1,20 @@
 #pragma GCC optimize("O3")
 class Solution {
 public:
-    int findLeastNumOfUniqueInts(vector<int>& arr, int k) 
-    {
-        ios_base::sync_with_stdio(false);
-        
-        unordered_map<int, int> um;
-        for (auto it : arr) um[it]++;
-        
-        vector<pair<int,int>> freq;
-        
-        for(auto it: um)freq.push_back({it.second,it.first});
-        
-        sort(freq.begin(),freq.end());
-    
-        for (auto it : freq)
-        {
-            if (it.first > k) 
-                return um.size();
-            k -= it.first;
-            um.erase(it.second);
-        }
-        
-        return um.size();
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+    unordered_map<int, int> counts;
+    vector<int> heap;
+    for (auto n : arr)
+        ++counts[n];
+    for (auto &p: counts)
+        heap.push_back(p.second);
+    make_heap(begin(heap), end(heap), greater<int>());
+    while (k > 0) {
+        k -= heap.front();
+        pop_heap(begin(heap), end(heap), greater<int>()); 
+        if (k >= 0)
+            heap.pop_back();
     }
+    return heap.size();
+}
 };
