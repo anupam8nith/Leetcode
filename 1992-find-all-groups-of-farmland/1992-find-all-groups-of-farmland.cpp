e8512161499a03492f51110aacf7654d5f42fd48
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-04-21 00:08:13 +0530

    Time: 73 ms (99.64%), Space: 59.4 MB (84.36%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
-    int row, col;
-
-    void findBoundaries(vector<vector<int>>& land, int x, int y, pair<int, int>& c1, pair<int, int>& c2) {
-        queue<pair<int, int>> q;
-        q.push({x, y});
-        land[x][y] = 0;
-
-        while (!q.empty()) {
-            auto [curX, curY] = q.front();
-            q.pop();
-
-            c1.first = min(c1.first, curX);
-            c1.second = min(c1.second, curY);
-            c2.first = max(c2.first, curX);
-            c2.second = max(c2.second, curY);
-
-            vector<int> dx = { -1, 0, 0, 1};
-            vector<int> dy = {0, -1, 1, 0};
-
-            for (int k = 0; k < 4; k++) {
-                int newX = curX + dx[k];
-                int newY = curY + dy[k];
-                if (newX >= 0 && newX < row && newY >= 0 && newY < col && land[newX][newY] == 1) {
-                    q.push({newX, newY});
-                    land[newX][newY] = 0;
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        int n = land.size();
+        int m = land[0].size();
+        vector<vector<int>> result;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!land[i][j]) continue;
+                
+                int c2 = j;
+                while(c2<m && land[i][c2]){
+                    c2++;
+                }
+                int r2 = i;
+                while(r2<n && land[r2][j]){
+                    r2++;
+                }
+                c2 = !c2 ? c2 : c2-1;
+                r2 = !r2 ? r2 : r2-1;
+                result.push_back({i,j,r2,c2});
+                
+                for(int k=i; k<=r2; k++){
+                    for(int l=j; l<=c2; l++){
+                        land[k][l]=0;
+                    }
                 }
             }
         }
-    }
-
-    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        vector<vector<int>> ans; 
-        row = land.size(); 
-        col = land[0].size();
-
-        for (int i = 0; i < row; i++)
-            for (int j = 0; j < col; j++) {
-                if (land[i][j] == 1) {
-                    pair<int, int> c1 = {i, j}, c2 = {i, j};
-                    findBoundaries(land, i, j, c1, c2);
-                    ans.push_back({c1.first, c1.second, c2.first, c2.second});
-                }
-            }
-
-        return ans;
+        return result;
     }
 };
