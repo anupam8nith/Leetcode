112c20b4a84fe79418d24bc2e8cd06d34793072a
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2024-04-20 09:37:51 +0530

    Time: 247 ms (22.18%), Space: 163.7 MB (5.46%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-
-
     int row, col;
 
-    void bfs(vector<vector<int>>& land, int x, int y, pair<int, int> &c1, pair<int, int> &c2)
-    {
-        if (x < 0 || x >= row || y < 0 || y >= col || land[x][y] == 0) return;
-
+    void findBoundaries(vector<vector<int>>& land, int x, int y, pair<int, int>& c1, pair<int, int>& c2) {
+        queue<pair<int, int>> q;
+        q.push({x, y});
         land[x][y] = 0;
-        if(c1.first>=x && c1.second>=y) {c1.first=x;c1.second=y;}
-        if(c2.first<=x && c2.second<=y) {c2.first=x;c2.second=y;}
 
-        vector<int> dx = { -1, 0, 0, 1};
-        vector<int> dy = {0, -1, 1, 0};
+        while (!q.empty()) {
+            auto [curX, curY] = q.front();
+            q.pop();
 
-        for (int k = 0; k < 4; k++)
-            bfs(land, x + dx[k], y + dy[k],c1,c2);
+            c1.first = min(c1.first, curX);
+            c1.second = min(c1.second, curY);
+            c2.first = max(c2.first, curX);
+            c2.second = max(c2.second, curY);
 
+            vector<int> dx = { -1, 0, 0, 1};
+            vector<int> dy = {0, -1, 1, 0};
+
+            for (int k = 0; k < 4; k++) {
+                int newX = curX + dx[k];
+                int newY = curY + dy[k];
+                if (newX >= 0 && newX < row && newY >= 0 && newY < col && land[newX][newY] == 1) {
+                    q.push({newX, newY});
+                    land[newX][newY] = 0;
+                }
+            }
+        }
     }
 
-    vector<vector<int>> findFarmland(vector<vector<int>>& land)
-    {
-        vector<vector<int>> ans; row = size(land); col = land[0].size();
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        vector<vector<int>> ans; 
+        row = land.size(); 
+        col = land[0].size();
 
         for (int i = 0; i < row; i++)
-            for (int j = 0; j < col; j++)
-            {
-                if (land[i][j] == 1)
-                {
-                    pair<int, int> c1 = {row, col}, c2 = { -1, -1};
-                    bfs(land, i, j, c1, c2);
-                    vector<int> v = {c1.first, c1.second, c2.first, c2.second};
-                    ans.push_back(v);
+            for (int j = 0; j < col; j++) {
+                if (land[i][j] == 1) {
+                    pair<int, int> c1 = {i, j}, c2 = {i, j};
+                    findBoundaries(land, i, j, c1, c2);
+                    ans.push_back({c1.first, c1.second, c2.first, c2.second});
                 }
             }
 
