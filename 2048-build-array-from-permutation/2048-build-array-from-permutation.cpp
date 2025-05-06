25073fed7b1fa8e4ac39c92b3c7305f93550056a
ANUPAM KUMAR <87599831+anupam8nith@users.noreply.github.com>
2025-05-06 20:58:04 +0530

    Time: 0 ms (100%), Space: 20.4 MB (58.11%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> buildArray(vector<int>& nums) {
         int n = size(nums);
         for(int i=0;i<n;i++)
-            {
+            {   //We add something to the existing number so that when we divide it, we get the quotient as the desired number
                 int new_value = nums[nums[i]]%n;
                 nums[i] += new_value*n;
             }
